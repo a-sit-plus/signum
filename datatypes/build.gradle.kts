566ab96b0391497c46d0ea870e3f8a42d3e83c36
Bernd Prünster <bernd.pruenster@a-sit.at>
2023-11-06 09:57:03 +0100

    kotlin 1.9.20

--- datatypes/build.gradle.kts
+++ datatypes/build.gradle.kts
@@ -12,10 +12,13 @@ plugins {
 version = "2.1.0-SNAPSHOT"
 
 
-exportIosFramework("KmpCrypto", serialization("json"), datetime(), "at.asitplus:kmmresult:${kmmresult}")
 kotlin {
+    jvm()
+    iosArm64()
+    iosSimulatorArm64()
+    iosX64()
     sourceSets {
-        val commonMain by getting {
+        commonMain {
             dependencies {
                 api("at.asitplus:kmmresult:${kmmresult}")
                 api(serialization("json"))
@@ -23,24 +26,23 @@ kotlin {
                 implementation("io.matthewnelson.kotlin-components:encoding-base16:${encoding}")
                 implementation("io.matthewnelson.kotlin-components:encoding-base64:${encoding}")
             }
-            val commonTest by getting {
-                dependencies {
-                    implementation(kotest("property"))
-                    implementation(kotlin("reflect"))
-                }
+        }
+
+        commonTest {
+            dependencies {
+                implementation(kotest("property"))
+                implementation(kotlin("reflect"))
             }
         }
 
-        val jvmMain by getting {
+        jvmMain {
             dependencies {
                 api(bouncycastle("bcpkix"))
             }
         }
-
-        val commonTest by getting
-        val jvmTest by getting
     }
 }
+exportIosFramework("KmpCrypto", serialization("json"), datetime(), "at.asitplus:kmmresult:${kmmresult}")
 
 val javadocJar = setupDokka(baseUrl = "https://github.com/a-sit-plus/kmp-crypto/tree/main/", multiModuleDoc = true)
 
