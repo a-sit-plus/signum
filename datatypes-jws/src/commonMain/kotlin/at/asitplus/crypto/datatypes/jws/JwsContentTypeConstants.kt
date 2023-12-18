a68f6d6269693a83e581daf27a63d192c520ea85
Bernd Prünster <bernd.pruenster@a-sit.at>
2023-12-18 09:46:44 +0100

    remove vclib-specific constants

--- datatypes-jws/src/commonMain/kotlin/at/asitplus/crypto/datatypes/jws/JwsContentTypeConstants.kt
+++
@@ -1,9 +0,0 @@
-package at.asitplus.crypto.datatypes.jws
-
-
-object JwsContentTypeConstants {
-    const val JWT = "jwt"
-    const val DIDCOMM_PLAIN_JSON = "didcomm-plain+json"
-    const val DIDCOMM_SIGNED_JSON = "didcomm-signed+json"
-    const val DIDCOMM_ENCRYPTED_JSON = "didcomm-encrypted+json"
-}
