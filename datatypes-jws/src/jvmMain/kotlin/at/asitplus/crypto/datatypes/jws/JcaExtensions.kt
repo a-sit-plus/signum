702478e84a3aaf615497989b52e278e79e165fe4
Christian Kollmann <christian.kollmann@a-sit.at>
2024-06-10 20:35:00 +0200

    JCA: Use padding for CBC

--- datatypes-jws/src/jvmMain/kotlin/at/asitplus/crypto/datatypes/jws/JcaExtensions.kt
+++ datatypes-jws/src/jvmMain/kotlin/at/asitplus/crypto/datatypes/jws/JcaExtensions.kt
@@ -3,7 +3,13 @@ package at.asitplus.crypto.datatypes.jws
 val JweEncryption.jcaName
     get() = when (this) {
         JweEncryption.A128GCM, JweEncryption.A192GCM, JweEncryption.A256GCM -> "AES/GCM/NoPadding"
-        JweEncryption.A128CBC_HS256, JweEncryption.A192CBC_HS384, JweEncryption.A256CBC_HS512 -> "AES/CBC/NoPadding"
+        JweEncryption.A128CBC_HS256, JweEncryption.A192CBC_HS384, JweEncryption.A256CBC_HS512 -> "AES/CBC/PKCS5Padding"
+    }
+
+val JweEncryption.isAuthenticatedEncryption
+    get() = when (this) {
+        JweEncryption.A128GCM, JweEncryption.A192GCM, JweEncryption.A256GCM -> true
+        JweEncryption.A128CBC_HS256, JweEncryption.A192CBC_HS384, JweEncryption.A256CBC_HS512 -> false
     }
 
 val JweEncryption.jcaKeySpecName
