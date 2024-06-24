f267b15bf5866b5ff429919b448a991b8be5d721
Christian Kollmann <christian.kollmann@a-sit.at>
2024-06-24 15:21:13 +0200

    CBOR: Add "COSE_Key" to COSE header

--- datatypes-cose/src/commonTest/kotlin/at/asitplus/crypto/datatypes/cose/CoseSerializationTest.kt
+++ datatypes-cose/src/commonTest/kotlin/at/asitplus/crypto/datatypes/cose/CoseSerializationTest.kt
@@ -12,6 +12,8 @@ import io.matthewnelson.encoding.core.Decoder.Companion.decodeToByteArray
 import io.matthewnelson.encoding.core.Encoder.Companion.encodeToString
 import kotlinx.serialization.cbor.ByteStringWrapper
 
+val Base16Strict = Base16(strict = true)
+
 class CoseSerializationTest : FreeSpec({
 
     Napier.base(DebugAntilog())
@@ -23,7 +25,7 @@ class CoseSerializationTest : FreeSpec({
             payload = "This is the content.".encodeToByteArray(),
             signature = CryptoSignature.RSAorHMAC("bar".encodeToByteArray()) //RSAorHMAC because EC expects tuple
         )
-        val serialized = cose.serialize().encodeToString(Base16(strict = true)).uppercase()
+        val serialized = cose.serialize().encodeToString(Base16Strict).uppercase()
 
         serialized shouldContain "546869732069732074686520636F6E74656E742E" // "This is the content."
         serialized shouldContain "43A10126"
@@ -32,7 +34,7 @@ class CoseSerializationTest : FreeSpec({
     "Serialize header" {
         val header = CoseHeader(algorithm = CoseAlgorithm.ES256, kid = "11".encodeToByteArray())
 
-        header.serialize().encodeToString(Base16(strict = true)).uppercase()
+        header.serialize().encodeToString(Base16Strict).uppercase()
             .also { println(it) }
 
         val deserialized = CoseHeader.deserialize(header.serialize()).getOrThrow().shouldNotBeNull()
@@ -44,11 +46,12 @@ class CoseSerializationTest : FreeSpec({
     "Serialize header with COSE_Key" {
         val header = CoseHeader(
             algorithm = CoseAlgorithm.ES256,
-            kid = "11".encodeToByteArray()
+            coseKey = "foo".encodeToByteArray(),
         )
 
-        header.serialize().encodeToString(Base16(strict = true)).uppercase()
+        val serialized = header.serialize().encodeToString(Base16Strict).uppercase()
             .also { println(it) }
+        serialized shouldContain "COSE_Key".encodeToByteArray().encodeToString(Base16Strict)
 
         val deserialized = CoseHeader.deserialize(header.serialize()).getOrThrow().shouldNotBeNull()
 
@@ -61,7 +64,7 @@ class CoseSerializationTest : FreeSpec({
                 "742e58408eb33e4ca31d1c465ab05aac34cc6b23d58fef5c083106c4d25a" +
                 "91aef0b0117e2af9a291aa32e14ab834dc56ed2a223444547e01f11d3b09" +
                 "16e5a4c345cacb36"
-        val cose = CoseSigned.deserialize(input.uppercase().decodeToByteArray(Base16(strict = true)))
+        val cose = CoseSigned.deserialize(input.uppercase().decodeToByteArray(Base16Strict))
 
         println(cose)
         cose.shouldNotBeNull()
