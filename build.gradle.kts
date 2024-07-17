22a6418e23c21342139de649a62fa9e246a4b6fe
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-07-17 18:04:41 +0200

    3.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
 
 plugins {
-    id("at.asitplus.gradle.conventions") version "2.0.0+20240610"
+    id("at.asitplus.gradle.conventions") version "2.0.0+20240717"
 }
 group = "at.asitplus.crypto"
 
