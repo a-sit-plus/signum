5b612d1a74dc734e25bf0fec74c133f61049a485
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-08-29 20:18:32 +0200

    fix demoapp build

--- demoapp/composeApp/build.gradle.kts
+++ demoapp/composeApp/build.gradle.kts
@@ -3,6 +3,7 @@ import com.android.build.api.dsl.Packaging
 plugins {
     alias(libs.plugins.multiplatform)
     alias(libs.plugins.compose)
+    alias(libs.plugins.compose.runtime)
     alias(libs.plugins.android.application)
     alias(libs.plugins.buildConfig)
 }
