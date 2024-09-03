5c6b68a055ca234e03a190b73edb47d687c199fd
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-09-03 08:12:54 +0200

    fix comment in build file

--- demoapp/settings.gradle.kts
+++ demoapp/settings.gradle.kts
@@ -7,9 +7,9 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
 
-        //required for indispensable modules
+        //required for indispensable modules composite build
         maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
-        //required for indispensable modules
+        //required for indispensable modules composite build
         maven {
             url =
                 uri("https://raw.githubusercontent.com/a-sit-plus/gradle-conventions-plugin/mvn/repo")
