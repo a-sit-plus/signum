4543549170688d1f434551cfdc8527be43de9fe5
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-09-30 22:30:45 +0200

    introduce tbscsr and tbscert signing shorthands

--- supreme/src/commonTest/kotlin/at/asitplus/signum/supreme/sign/EphemeralSignerCommonTests.kt
+++ supreme/src/commonTest/kotlin/at/asitplus/signum/supreme/sign/EphemeralSignerCommonTests.kt
@@ -1,10 +1,12 @@
 package at.asitplus.signum.supreme.sign
 
-import at.asitplus.signum.indispensable.Digest
-import at.asitplus.signum.indispensable.ECCurve
-import at.asitplus.signum.indispensable.RSAPadding
-import at.asitplus.signum.indispensable.SignatureAlgorithm
-import at.asitplus.signum.indispensable.nativeDigest
+import at.asitplus.signum.indispensable.*
+import at.asitplus.signum.indispensable.asn1.Asn1PrimitiveOctetString
+import at.asitplus.signum.indispensable.asn1.Asn1String
+import at.asitplus.signum.indispensable.asn1.Asn1Time
+import at.asitplus.signum.indispensable.asn1.KnownOIDs
+import at.asitplus.signum.indispensable.pki.*
+import at.asitplus.signum.supreme.sign
 import at.asitplus.signum.supreme.signature
 import at.asitplus.signum.supreme.succeed
 import com.ionspin.kotlin.bignum.integer.Quadruple
@@ -16,7 +18,9 @@ import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNot
 import io.kotest.matchers.types.shouldBeInstanceOf
+import kotlinx.datetime.Clock
 import kotlin.random.Random
+import kotlin.time.Duration.Companion.days
 
 class EphemeralSignerCommonTests : FreeSpec({
     "Functional" - {
@@ -135,4 +139,151 @@ class EphemeralSignerCommonTests : FreeSpec({
             }
         }
     }
+
+    "Cert signing" - {
+        "RSA" - {
+            withData(
+                nameFn = { (pad, dig, bits, pre) -> "$dig/$pad/${bits}bit${if (pre) "/pre" else ""}" },
+                sequence {
+                    RSAPadding.entries.forEach { padding ->
+                        Digest.entries.forEach { digest ->
+                            when {
+                                digest == Digest.SHA512 && padding == RSAPadding.PSS
+                                    -> listOf(2048, 3072, 4096)
+
+                                digest == Digest.SHA384 || digest == Digest.SHA512 || padding == RSAPadding.PSS
+                                    -> listOf(1024, 2048, 3072, 4096)
+
+                                else
+                                    -> listOf(512, 1024, 2048, 3072, 4096)
+                            }.forEach { keySize ->
+                                yield(Quadruple(padding, digest, keySize, false))
+                                yield(Quadruple(padding, digest, keySize, true))
+                            }
+                        }
+                    }
+                }) { (padding, digest, keySize, preHashed) ->
+                val data = Random.Default.nextBytes(64)
+                val signer: Signer
+
+
+                try {
+                    signer = Signer.Ephemeral {
+                        rsa {
+                            digests = setOf(digest); paddings = setOf(padding); bits = keySize
+                        }
+                    }.getOrThrow()
+                    signer.sign(SignatureInput(data).let {
+                        if (preHashed) it.convertTo(digest).getOrThrow() else it
+                    }).signature
+                } catch (x: UnsupportedOperationException) {
+                    return@withData
+                }
+
+                val csr = TbsCertificationRequest(
+                    subjectName = listOf(RelativeDistinguishedName(AttributeTypeAndValue.CommonName(Asn1String.UTF8("client")))),
+                    publicKey = signer.publicKey,
+                    attributes = listOf(
+                        Pkcs10CertificationRequestAttribute(
+                            // No OID is assigned for this; choose one!
+                            KnownOIDs.id_sMIME,
+                            // ↓↓↓ contains challenge ↓↓↓
+                            Asn1String.UTF8("foo").encodeToTlv()
+                        )
+                    )
+                )
+                if(digest == Digest.SHA1 && padding== RSAPadding.PSS) return@withData
+                val signedCSR = signer.sign(csr).getOrThrow()
+
+
+                val verifier = signer.makeVerifier().getOrThrow()
+                verifier.verify(signedCSR.tbsCsr.encodeToDer(), signedCSR.signature) should succeed
+
+
+                val tbsCrt = TbsCertificate(
+                    serialNumber = Random.nextBytes(16),
+                    signatureAlgorithm = signer.signatureAlgorithm.toX509SignatureAlgorithm().getOrThrow(),
+                    issuerName = listOf(RelativeDistinguishedName(AttributeTypeAndValue.CommonName(Asn1String.UTF8("Foo")))),
+                    validFrom = Asn1Time(
+                        Clock.System.now()
+                    ),
+                    validUntil = Asn1Time(Clock.System.now() + 356.days),
+                    subjectName = listOf(RelativeDistinguishedName(AttributeTypeAndValue.CommonName(Asn1String.UTF8("client")))),
+                    publicKey = signer.publicKey,
+                    extensions = listOf(
+                        X509CertificateExtension(
+                            KnownOIDs.pkcs_12_OID,
+                            critical = true,
+                            Asn1PrimitiveOctetString(byteArrayOf())
+                        )
+                    )
+                )
+                val cert = signer.sign(tbsCrt).getOrThrow()
+
+                verifier.verify(cert.tbsCertificate.encodeToDer(), cert.signature) should succeed
+
+            }
+        }
+
+        "ECDSA" - {
+            withData(
+                nameFn = { (crv, dig, pre) -> "$crv/$dig${if (pre) "/pre" else ""}" },
+                sequence {
+                    ECCurve.entries.forEach { curve ->
+                        Digest.entries.filterNot { it == Digest.SHA1 }.forEach { digest ->
+                            yield(Triple(curve, digest, false))
+                            yield(Triple(curve, digest, true))
+                        }
+                    }
+                }) { (crv, digest, preHashed) ->
+                val signer =
+                    Signer.Ephemeral { ec { curve = crv; digests = setOf(digest) } }.getOrThrow()
+                signer.signatureAlgorithm.shouldBeInstanceOf<SignatureAlgorithm.ECDSA>().let {
+                    it.digest shouldBe digest
+                    it.requiredCurve shouldBeIn setOf(null, crv)
+                }
+                val csr = TbsCertificationRequest(
+                    subjectName = listOf(RelativeDistinguishedName(AttributeTypeAndValue.CommonName(Asn1String.UTF8("client")))),
+                    publicKey = signer.publicKey,
+                    attributes = listOf(
+                        Pkcs10CertificationRequestAttribute(
+                            // No OID is assigned for this; choose one!
+                            KnownOIDs.id_sMIME,
+                            // ↓↓↓ contains challenge ↓↓↓
+                            Asn1String.UTF8("foo").encodeToTlv()
+                        )
+                    )
+                )
+                val signedCSR = signer.sign(csr).getOrThrow()
+
+
+                val verifier = signer.makeVerifier().getOrThrow()
+                verifier.verify(signedCSR.tbsCsr.encodeToDer(), signedCSR.signature) should succeed
+
+
+                val tbsCrt = TbsCertificate(
+                    serialNumber = Random.nextBytes(16),
+                    signatureAlgorithm = signer.signatureAlgorithm.toX509SignatureAlgorithm().getOrThrow(),
+                    issuerName = listOf(RelativeDistinguishedName(AttributeTypeAndValue.CommonName(Asn1String.UTF8("Foo")))),
+                    validFrom = Asn1Time(
+                        Clock.System.now()
+                    ),
+                    validUntil = Asn1Time(Clock.System.now() + 356.days),
+                    subjectName = listOf(RelativeDistinguishedName(AttributeTypeAndValue.CommonName(Asn1String.UTF8("client")))),
+                    publicKey = signer.publicKey,
+                    extensions = listOf(
+                        X509CertificateExtension(
+                            KnownOIDs.pkcs_12_OID,
+                            critical = true,
+                            Asn1PrimitiveOctetString(byteArrayOf())
+                        )
+                    )
+                )
+                val cert = signer.sign(tbsCrt).getOrThrow()
+
+                verifier.verify(cert.tbsCertificate.encodeToDer(), cert.signature) should succeed
+            }
+        }
+    }
+
 })
