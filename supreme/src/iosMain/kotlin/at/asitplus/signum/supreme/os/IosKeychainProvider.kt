7228ba381bbe0d53d3c880894e178c63348aef54
Jakob Heher <jakob.heher@iaik.tugraz.at>
2024-10-29 14:24:44 +0100

    type hierarchy: all platform signing provider signers are attestable (#178)

--- supreme/src/iosMain/kotlin/at/asitplus/signum/supreme/os/IosKeychainProvider.kt
+++ supreme/src/iosMain/kotlin/at/asitplus/signum/supreme/os/IosKeychainProvider.kt
@@ -179,7 +179,7 @@ typealias IosSignerSigningConfiguration = PlatformSigningProviderSignerSigningCo
 sealed class IosSigner(final override val alias: String,
                        private val metadata: IosKeyMetadata,
                        private val signerConfig: IosSignerConfiguration)
-    : PlatformSigningProviderSigner<IosSignerSigningConfiguration>, Signer.Attestable<IosHomebrewAttestation> {
+    : PlatformSigningProviderSigner<IosSignerSigningConfiguration, IosHomebrewAttestation> {
 
     override val mayRequireUserUnlock get() = needsAuthentication
     val needsAuthentication get() = metadata.needsUnlock
