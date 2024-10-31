a01c0cc5e06f5716f3538d2071fd21469cab5091
Christian Kollmann <christian.kollmann@a-sit.at>
2024-10-31 09:16:17 +0100

    Add falback operations with ByteArray for JwsSigned

--- indispensable-josef/src/commonMain/kotlin/at/asitplus/signum/indispensable/josef/JwsSigned.kt
+++ indispensable-josef/src/commonMain/kotlin/at/asitplus/signum/indispensable/josef/JwsSigned.kt
@@ -3,6 +3,7 @@ package at.asitplus.signum.indispensable.josef
 import at.asitplus.KmmResult
 import at.asitplus.catching
 import at.asitplus.signum.indispensable.CryptoSignature
+import at.asitplus.signum.indispensable.ECCurve
 import at.asitplus.signum.indispensable.io.Base64UrlStrict
 import io.matthewnelson.encoding.core.Decoder.Companion.decodeToByteArray
 import io.matthewnelson.encoding.core.Encoder.Companion.encodeToString
@@ -54,25 +55,63 @@ data class JwsSigned<out P : Any>(
 
 
     companion object {
-        inline fun <reified P : Any> deserialize(it: String, json: Json = Json): KmmResult<JwsSigned<P>> = catching {
-            val stringList = it.replace("[^A-Za-z0-9-_.]".toRegex(), "").split(".")
-            if (stringList.size != 3) throw IllegalArgumentException("not three parts in input: $it")
-            val headerInput = stringList[0].decodeToByteArray(Base64UrlStrict)
-            val header = JwsHeader.deserialize(headerInput.decodeToString())
-                .mapFailure { it.apply { printStackTrace() } }
-                .getOrThrow()
-            val payload: P = json.decodeFromString<P>(stringList[1].decodeToByteArray(Base64UrlStrict).decodeToString())
-            val signature = stringList[2].decodeToByteArray(Base64UrlStrict)
-                .let { bytes ->
-                    when (val curve = header.algorithm.ecCurve) {
-                        null -> CryptoSignature.RSAorHMAC(bytes)
-                        else -> CryptoSignature.EC.fromRawBytes(curve, bytes)
-                    }
-                }
+        /**
+         * Deserializes the input, expected to contain a valid JWS (three Base64-URL strings joined by `.`),
+         * into a [JwsSigned] with `ByteArray` as the type of the payload.
+         */
+        inline fun deserialize(it: String): KmmResult<JwsSigned<ByteArray>> = catching {
+            val stringList = it.parseTo3Parts()
+            val inputParts = stringList.map { it.decodeToByteArray(Base64UrlStrict) }
+            val header = inputParts[0].toJwsHeader()
+            val payload = inputParts[1]
+            val signature = inputParts[2].toSignature(header.algorithm.ecCurve)
             val plainSignatureInput = stringList[0] + "." + stringList[1]
             JwsSigned(header, payload, signature, plainSignatureInput)
         }
 
+        /**
+         * Deserializes the input, expected to contain a valid JWS (three Base64-URL strings joined by `.`),
+         * into a [JwsSigned] with [P] as the type of the payload.
+         */
+        inline fun <reified P : Any> deserialize(it: String, json: Json = Json): KmmResult<JwsSigned<P>> = catching {
+            val stringList = it.parseTo3Parts()
+            val inputParts = stringList.map { it.decodeToByteArray(Base64UrlStrict) }
+            val header = inputParts[0].toJwsHeader()
+            val payload: P = inputParts[1].run { json.decodeFromString<P>(this.decodeToString()) }
+            val signature = inputParts[2].toSignature(header.algorithm.ecCurve)
+            val plainSignatureInput = stringList[0] + "." + stringList[1]
+            JwsSigned(header, payload, signature, plainSignatureInput)
+        }
+
+        fun String.parseTo3Parts(): List<String> {
+            val stringList = replace("[^A-Za-z0-9-_.]".toRegex(), "").split(".")
+            if (stringList.size != 3)
+                throw IllegalArgumentException("not three parts in input: $this")
+            return stringList
+        }
+
+        fun ByteArray.toJwsHeader(): JwsHeader =
+            JwsHeader.deserialize(decodeToString())
+                .mapFailure { it.apply { printStackTrace() } }
+                .getOrThrow()
+
+        fun ByteArray.toSignature(ecCurve: ECCurve?) =
+            when (ecCurve) {
+                null -> CryptoSignature.RSAorHMAC(this)
+                else -> CryptoSignature.EC.fromRawBytes(ecCurve, this)
+            }
+
+        /**
+         * Called by JWS signing implementations to get the string that will be
+         * used as the input for signature calculation
+         */
+        @Suppress("unused")
+        inline fun prepareJwsSignatureInput(
+            header: JwsHeader,
+            payload: ByteArray,
+            json: Json = Json,
+        ): String = "${header.serialize().encodeToByteArray().encodeToString(Base64UrlStrict)}" +
+                ".${payload.encodeToString(Base64UrlStrict)}"
 
         /**
          * Called by JWS signing implementations to get the string that will be
