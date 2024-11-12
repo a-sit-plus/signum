a23dd845600636ad0fdf1d42f454942a323a1296
Jakob Heher <jakob.heher@iaik.tugraz.at>
2024-11-12 11:47:58 +0100

    add a test for password-protected JKS private keys
    also improve documentation

--- supreme/src/jvmTest/kotlin/at/asitplus/signum/supreme/os/JKSProviderTest.kt
+++ supreme/src/jvmTest/kotlin/at/asitplus/signum/supreme/os/JKSProviderTest.kt
@@ -10,8 +10,10 @@ import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNot
 import io.kotest.property.azstring
 import java.nio.file.Files
+import java.nio.file.Path
 import kotlin.random.Random
 
+@OptIn(ExperimentalStdlibApi::class)
 class JKSProviderTest : FreeSpec({
     "Ephemeral" {
         val ks = JKSProvider.Ephemeral().getOrThrow()
@@ -23,6 +25,27 @@ class JKSProviderTest : FreeSpec({
         val data = Random.Default.nextBytes(64)
         val signature = signer.sign(data).signature
         otherSigner.makeVerifier().getOrThrow().verify(data, signature) should succeed
+    }
+    "Key With Password" {
+        val ks = JKSProvider {
+            file {
+                file = Path.of("src/jvmTest/resources/DoorsOfDurin.p12")
+                password = "password".toCharArray()
+
+                /* test auto-detection */
+                storeType shouldBe "pkcs12"
+            }
+        }.getOrThrow()
+        val alias = "Moria"
+        val correctKeyPassword = "Mellon".toCharArray()
+        val wrongKeyPassword = "Edro".toCharArray()
+        ks.getSignerForKey(alias) shouldNot succeed
+        ks.getSignerForKey(alias) { privateKeyPassword = wrongKeyPassword } shouldNot succeed
+        val signer = ks.getSignerForKey(alias) { privateKeyPassword = correctKeyPassword }.getOrThrow()
+        signer.publicKey.encodeToDer().toHexString(HexFormat.UpperCase) shouldBe
+                "3059301306072A8648CE3D020106082A8648CE3D030107034200046EEDD7DCE99AA264797906CE55BC158E4" +
+                "22EA9722E7EB0F0A6C7C9AB53F4B0D09176D8D169F52872BE2ED31D33C9ABD5785BB1DF96F53213BA659636" +
+                "96527B09"
     }
     "File-based persistence" {
         val tempfile = Files.createTempFile(Random.azstring(16),null).also { Files.delete(it) }
