630be05719ca6af0c7b3b110bd27e5eebdc026f0
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-11-13 16:56:25 +0100

    sketch swift gcm

--- supreme/src/iosTest/kotlin/Test.kt
+++ supreme/src/iosTest/kotlin/Test.kt
@@ -1,9 +1,21 @@
+import at.asitplus.signum.indispensable.EncryptionAlgorithm
+import at.asitplus.signum.supreme.crypt.Encryptor
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldNotBe
+import kotlin.random.Random
 
+@ExperimentalStdlibApi
 class ProviderTest : FreeSpec({
 
     "This dummy test" {
         "is just making sure" shouldNotBe "that iOS tests are indeed running"
+    }
+
+    "AES" {
+        val key = Random.Default.nextBytes(16)
+        val iv = Random.Default.nextBytes(16)
+        val aad = Random.Default.nextBytes(16)
+        println( Encryptor(EncryptionAlgorithm.AES128_GCM,key, iv, aad).encrypt("1337".encodeToByteArray()).map { it.toHexString(
+            HexFormat.UpperCase) }.onFailure { it.printStackTrace() })
     }
 })
