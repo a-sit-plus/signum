7c60d4ea611286e5baec2a2a6828aece16039f93
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-11-20 00:14:40 +0100

    JVM decryption working

--- indispensable/src/commonMain/kotlin/at/asitplus/signum/indispensable/Ciphertext.kt
+++ indispensable/src/commonMain/kotlin/at/asitplus/signum/indispensable/Ciphertext.kt
@@ -1,17 +1,17 @@
 package at.asitplus.signum.indispensable
 
-open class Ciphertext(val encryptedData: ByteArray, val iv: ByteArray? = null) {
+open class Ciphertext(val algorithm: EncryptionAlgorithm, val encryptedData: ByteArray, val iv: ByteArray? = null) {
 
     @OptIn(ExperimentalStdlibApi::class)
     override fun toString(): String =
-        "Ciphertext(encryptedData=${encryptedData.toHexString(HexFormat.UpperCase)}, iv=${iv?.toHexString(HexFormat.UpperCase)})"
+        "$algorithm Ciphertext(encryptedData=${encryptedData.toHexString(HexFormat.UpperCase)}, iv=${iv?.toHexString(HexFormat.UpperCase)})"
 
-    class Authenticated(encryptedData: ByteArray, iv: ByteArray? = null, val authTag: ByteArray, val aad: ByteArray?) :
-        Ciphertext(encryptedData, iv) {
+    class Authenticated(algorithm: EncryptionAlgorithm, encryptedData: ByteArray, iv: ByteArray? = null, val authTag: ByteArray, val aad: ByteArray?) :
+        Ciphertext(algorithm, encryptedData, iv) {
 
         @OptIn(ExperimentalStdlibApi::class)
         override fun toString(): String =
-            "Authenticated Ciphertext(encryptedData=${this.encryptedData.toHexString(HexFormat.UpperCase)}, iv=${iv?.toHexString(HexFormat.UpperCase)}, authTag=${
+            " $algorithm Authenticated Ciphertext(encryptedData=${this.encryptedData.toHexString(HexFormat.UpperCase)}, iv=${iv?.toHexString(HexFormat.UpperCase)}, authTag=${
                 authTag.toHexString(HexFormat.UpperCase)
             }, aad=${aad?.toHexString(HexFormat.UpperCase)})"
     }
