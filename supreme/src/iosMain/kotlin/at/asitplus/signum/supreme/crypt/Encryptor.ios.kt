34adcd0140b450fe3c3f91327decee0ac7c41940
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-11-19 21:47:04 +0100

    AES GCM decryption working on iOS

--- supreme/src/iosMain/kotlin/at/asitplus/signum/supreme/crypt/Encryptor.ios.kt
+++ supreme/src/iosMain/kotlin/at/asitplus/signum/supreme/crypt/Encryptor.ios.kt
@@ -4,6 +4,7 @@ import at.asitplus.KmmResult
 import at.asitplus.signum.indispensable.Ciphertext
 import at.asitplus.signum.indispensable.EncryptionAlgorithm
 import at.asitplus.signum.supreme.aes.AESwift
+import at.asitplus.signum.supreme.swiftcall
 import at.asitplus.signum.supreme.toByteArray
 import at.asitplus.signum.supreme.toNSData
 import kotlinx.cinterop.ExperimentalForeignApi
@@ -31,14 +32,31 @@ actual internal fun PlatformCipher.encrypt(data: ByteArray): KmmResult<Ciphertex
     val ciphertext = AESwift.gcmWithPlain(nsData, nsKey, nsIV, nsAAD)
     if (ciphertext == null) return KmmResult.failure(UnsupportedOperationException("Error from swift code!"))
 
+
     return if (ciphertext.authTag() != null)
         KmmResult.success(
             Ciphertext.Authenticated(
                 ciphertext.ciphertext().toByteArray(),
                 ciphertext.iv().toByteArray(),
-                ciphertext.authTag()!!.toByteArray()
+                ciphertext.authTag()!!.toByteArray(),
+                aad
             )
         )
     else KmmResult.success(Ciphertext(ciphertext.ciphertext().toByteArray(), ciphertext.iv().toByteArray()))
 }
 
+
+@OptIn(ExperimentalForeignApi::class)
+internal fun decrypt(ciphertext: Ciphertext, key: ByteArray): ByteArray? {
+
+    return  swiftcall {
+        AESwift.gcmDecryptWithCiphertext(
+            ciphertext.encryptedData.toNSData(),
+            key.toNSData(),
+            ciphertext.iv!!.toNSData(),
+            (ciphertext as Ciphertext.Authenticated).authTag.toNSData(),
+            (ciphertext as Ciphertext.Authenticated).aad?.toNSData(),
+            error
+        )!!.toByteArray()
+    }
+}
