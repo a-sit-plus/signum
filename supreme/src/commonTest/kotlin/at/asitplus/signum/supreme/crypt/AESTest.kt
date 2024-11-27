069d7f3075e534b32226b4d5b8d6b40e0b1220d4
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-11-27 23:28:29 +0100

    Fixes

--- supreme/src/commonTest/kotlin/at/asitplus/signum/supreme/crypt/AESTest.kt
+++ supreme/src/commonTest/kotlin/at/asitplus/signum/supreme/crypt/AESTest.kt
@@ -5,6 +5,7 @@ import at.asitplus.signum.supreme.crypt.encryptorFor
 import at.asitplus.signum.supreme.crypt.randomKey
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.datatest.withData
+import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 import kotlin.random.Random
@@ -21,23 +22,31 @@ class AESTest : FreeSpec({
             ) {
 
                 val key = it.randomKey()
-                val iv = Random.Default.nextBytes((it.ivNumBits / 8u).toInt())
                 val plaintext = Random.Default.nextBytes(256)
 
-                println("KEY: ${key.toHexString()} IV: ${iv.toHexString()}  plaintext: ${plaintext.toHexString()}")
-
-                val ciphertext = it.encryptorFor(key, iv).getOrThrow().encrypt(plaintext).getOrThrow()
-
-
-                println(ciphertext)
-                ciphertext.iv shouldBe iv
-                ciphertext.shouldBeInstanceOf<Ciphertext.Unauthenticated>()
+                withData(
+                    nameFn = { "IV: " + it?.toHexString() },
+                    Random.Default.nextBytes((it.ivNumBits / 8u).toInt()),
+                    null
+                ) { iv ->
 
 
-                val decrypted = ciphertext.decrypt(key).getOrThrow()
-                println("DECRYPTED: " + decrypted.toHexString(HexFormat.UpperCase))
-                decrypted shouldBe plaintext
+                    println("KEY: ${key.toHexString()} IV: ${iv?.toHexString()}  plaintext: ${plaintext.toHexString()}")
 
+                    val ciphertext = it.encryptorFor(key, iv).getOrThrow().encrypt(plaintext).getOrThrow()
+
+
+                    println(ciphertext)
+                    ciphertext.iv.shouldNotBeNull()
+                    ciphertext.iv!!.size * 8 shouldBe it.ivNumBits.toInt()
+                    iv?.let { ciphertext.iv shouldBe it }
+                    ciphertext.shouldBeInstanceOf<Ciphertext.Unauthenticated>()
+
+
+                    val decrypted = ciphertext.decrypt(key).getOrThrow()
+                    println("DECRYPTED: " + decrypted.toHexString(HexFormat.UpperCase))
+                    decrypted shouldBe plaintext
+                }
             }
         }
     }
@@ -50,24 +59,32 @@ class AESTest : FreeSpec({
         ) {
 
             val key = it.randomKey()
-            val iv = Random.Default.nextBytes((it.ivNumBits / 8u).toInt())
-            val aad = Random.Default.nextBytes(32)
             val plaintext = Random.Default.nextBytes(256)
+            withData(
+                nameFn = { "IV: " + it?.toHexString() },
+                Random.Default.nextBytes((it.ivNumBits / 8u).toInt()),
+                null
+            ) { iv ->
 
-            println("KEY: ${key.toHexString()} IV: ${iv.toHexString()}  plaintext: ${plaintext.toHexString()}")
+                withData(nameFn = { "AAD: " + it?.toHexString() }, Random.Default.nextBytes(32), null) { aad ->
+                    println("KEY: ${key.toHexString()} IV: ${iv?.toHexString()}  plaintext: ${plaintext.toHexString()}")
 
-            val ciphertext = it.encryptorFor(key, iv, aad).getOrThrow().encrypt(plaintext).getOrThrow()
+                    val ciphertext = it.encryptorFor(key, iv, aad).getOrThrow().encrypt(plaintext).getOrThrow()
 
 
-            println(ciphertext)
-            ciphertext.iv shouldBe iv
-            ciphertext.shouldBeInstanceOf<Ciphertext.Authenticated>()
-            ciphertext.aad shouldBe aad
+                    println(ciphertext)
+                    ciphertext.iv.shouldNotBeNull()
+                    ciphertext.iv!!.size * 8 shouldBe it.ivNumBits.toInt()
 
-            val decrypted = ciphertext.decrypt(key).getOrThrow()
-            println("DECRYPTED: " + decrypted.toHexString(HexFormat.UpperCase))
-            decrypted shouldBe plaintext
+                    iv?.let { ciphertext.iv shouldBe it }
+                    ciphertext.shouldBeInstanceOf<Ciphertext.Authenticated>()
+                    ciphertext.aad shouldBe aad
 
+                    val decrypted = ciphertext.decrypt(key).getOrThrow()
+                    println("DECRYPTED: " + decrypted.toHexString(HexFormat.UpperCase))
+                    decrypted shouldBe plaintext
+                }
+            }
         }
     }
     "CBC+HMAC" - {
@@ -93,34 +110,41 @@ class AESTest : FreeSpec({
         ) {
 
             val key = it.randomKey()
-            val iv = Random.Default.nextBytes((it.ivNumBits / 8u).toInt())
-            val aad = Random.Default.nextBytes(32)
             val plaintext = Random.Default.nextBytes(256)
 
             withData(
+                nameFn = { "MAC KEY " + it.toHexString() },
                 Random.Default.nextBytes(8),
                 Random.Default.nextBytes(16),
                 Random.Default.nextBytes(32),
                 key
             ) { macKey ->
-                println("KEY: ${key.toHexString()} MACKEY: ${macKey.toHexString()} IV: ${iv.toHexString()}  plaintext: ${plaintext.toHexString()}")
 
-                val ciphertext =
-                    it.encryptorFor(key, macKey, iv, aad).getOrThrow().encrypt(plaintext)
-                        .getOrThrow()
+                withData(
+                    nameFn = { "IV: " + it?.toHexString() },
+                    Random.Default.nextBytes((it.ivNumBits / 8u).toInt()),
+                    null
+                ) { iv ->
+                    withData(nameFn = { "AAD: " + it?.toHexString() }, Random.Default.nextBytes(32), null) { aad ->
+                        println("KEY: ${key.toHexString()} MACKEY: ${macKey.toHexString()} IV: ${iv?.toHexString()}  plaintext: ${plaintext.toHexString()}")
+                        val ciphertext =
+                            it.encryptorFor(key, macKey, iv, aad).getOrThrow().encrypt(plaintext)
+                                .getOrThrow()
 
 
-                println(ciphertext)
-                ciphertext.iv shouldBe iv
-                ciphertext.shouldBeInstanceOf<Ciphertext.Authenticated.WithDedicatedMac>()
-                ciphertext.aad shouldBe aad
+                        println(ciphertext)
+                        iv?.let { ciphertext.iv shouldBe it }
+                        ciphertext.iv.shouldNotBeNull()
+                        ciphertext.shouldBeInstanceOf<Ciphertext.Authenticated.WithDedicatedMac>()
+                        ciphertext.aad shouldBe aad
 
-                val decrypted = ciphertext.decrypt(key, macKey).getOrThrow()
-                println("DECRYPTED: " + decrypted.toHexString(HexFormat.UpperCase))
-                decrypted shouldBe plaintext
+                        val decrypted = ciphertext.decrypt(key, macKey).getOrThrow()
+                        println("DECRYPTED: " + decrypted.toHexString(HexFormat.UpperCase))
+                        decrypted shouldBe plaintext
+                    }
+
+                }
             }
-
-
         }
     }
 })
