7ccd3e26a73066fd7ac78859fc7272d36b776fcb
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-11-28 18:26:37 +0100

    docs

--- supreme/src/androidMain/kotlin/at/asitplus/signum/supreme/crypt/Encryptor.android.kt
+++ supreme/src/androidMain/kotlin/at/asitplus/signum/supreme/crypt/Encryptor.android.kt
@@ -5,42 +5,40 @@ import at.asitplus.catching
 import at.asitplus.catchingUnwrapped
 import at.asitplus.signum.indispensable.AuthTrait
 import at.asitplus.signum.indispensable.Ciphertext
-import at.asitplus.signum.indispensable.EncryptionAlgorithm
-import at.asitplus.signum.supreme.sign.Signer
-import org.kotlincrypto.SecureRandom
+import at.asitplus.signum.indispensable.SymmetricEncryptionAlgorithm
 import javax.crypto.Cipher
 import javax.crypto.spec.GCMParameterSpec
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
-actual internal fun <T, A : AuthTrait, E : EncryptionAlgorithm<A>> initCipher(
+actual internal fun <T, A : AuthTrait, E : SymmetricEncryptionAlgorithm<A>> initCipher(
     algorithm: E,
     key: ByteArray,
     macKey: ByteArray?,
     iv: ByteArray?,
     aad: ByteArray?
 ): CipherParam<T,A> {
-    if(algorithm !is EncryptionAlgorithm.WithIV<*>) TODO()
+    if(algorithm !is SymmetricEncryptionAlgorithm.WithIV<*>) TODO()
     val nonce = iv ?: algorithm.randomIV()
     return Cipher.getInstance(algorithm.jcaName).apply {
-        if (algorithm is EncryptionAlgorithm.AES.GCM)
+        if (algorithm is SymmetricEncryptionAlgorithm.AES.GCM)
             init(
                 Cipher.ENCRYPT_MODE,
                 SecretKeySpec(key, algorithm.jcaKeySpec),
                 GCMParameterSpec(algorithm.tagNumBits.toInt(), nonce)
             )
-        else if(algorithm is EncryptionAlgorithm.AES.CBC<*>) //covers Plain and CBC, because CBC will delegate to here
+        else if(algorithm is SymmetricEncryptionAlgorithm.AES.CBC<*>) //covers Plain and CBC, because CBC will delegate to here
             init(
                 Cipher.ENCRYPT_MODE,
                 SecretKeySpec(key, algorithm.jcaKeySpec),
                 IvParameterSpec(nonce)
             )
         else TODO()
-        aad?.let { if(algorithm is EncryptionAlgorithm.AES.GCM) updateAAD(it) /*CBC-HMAC we do ourselves*/ }
+        aad?.let { if(algorithm is SymmetricEncryptionAlgorithm.AES.GCM) updateAAD(it) /*CBC-HMAC we do ourselves*/ }
     }.let { CipherParam<Cipher,A>(algorithm, it, macKey?:key, nonce, aad) as CipherParam<T, A> }
 }
 
-actual internal fun <A : AuthTrait> CipherParam<*,A>.encrypt(data: ByteArray): KmmResult<Ciphertext<A, EncryptionAlgorithm<A>>> {
+actual internal fun <A : AuthTrait> CipherParam<*,A>.doEncrypt(data: ByteArray): KmmResult<Ciphertext<A, SymmetricEncryptionAlgorithm<A>>> {
     (this as CipherParam<Cipher,A>)
     val jcaCiphertext =
         catchingUnwrapped { platformData.doFinal(data) }.getOrElse { return KmmResult.failure(it) }
@@ -54,22 +52,22 @@ actual internal fun <A : AuthTrait> CipherParam<*,A>.encrypt(data: ByteArray): K
             .toByteArray() else null
 
     return KmmResult.success(
-        if (authtag != null) Ciphertext.Authenticated(alg as EncryptionAlgorithm.Authenticated, ciphertext, iv, authtag, aad)
-        else Ciphertext.Unauthenticated(alg as EncryptionAlgorithm.Unauthenticated, ciphertext, iv)
-    ) as KmmResult<Ciphertext<A, EncryptionAlgorithm<A>>>
+        if (authtag != null) Ciphertext.Authenticated(alg as SymmetricEncryptionAlgorithm.Authenticated, ciphertext, iv, authtag, aad)
+        else Ciphertext.Unauthenticated(alg as SymmetricEncryptionAlgorithm.Unauthenticated, ciphertext, iv)
+    ) as KmmResult<Ciphertext<A, SymmetricEncryptionAlgorithm<A>>>
 }
 
-val EncryptionAlgorithm<*>.jcaName: String
+val SymmetricEncryptionAlgorithm<*>.jcaName: String
     get() = when (this) {
-        is EncryptionAlgorithm.AES.GCM -> "AES/GCM/NoPadding"
-        is EncryptionAlgorithm.AES.CBC -> "AES/CBC/PKCS5Padding"
+        is SymmetricEncryptionAlgorithm.AES.GCM -> "AES/GCM/NoPadding"
+        is SymmetricEncryptionAlgorithm.AES.CBC -> "AES/CBC/PKCS5Padding"
         //  is EncryptionAlgorithm.AES.ECB -> "AES/ECB/NoPadding"
         else -> TODO()
     }
 
-val EncryptionAlgorithm<*>.jcaKeySpec: String
+val SymmetricEncryptionAlgorithm<*>.jcaKeySpec: String
     get() = when (this) {
-        is EncryptionAlgorithm.AES -> "AES"
+        is SymmetricEncryptionAlgorithm.AES -> "AES"
         else -> TODO()
     }
 
