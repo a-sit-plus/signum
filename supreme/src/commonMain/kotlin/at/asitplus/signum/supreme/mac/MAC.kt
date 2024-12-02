73a4d851c3461690a3467d478ef2b2d4fda3d1c3
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-12-02 12:53:54 +0100

    more pr feedback

--- supreme/src/commonMain/kotlin/at/asitplus/signum/supreme/mac/MAC.kt
+++ supreme/src/commonMain/kotlin/at/asitplus/signum/supreme/mac/MAC.kt
@@ -11,9 +11,9 @@ import at.asitplus.signum.supreme.hash.digest
 fun MAC.mac(key: ByteArray, msg: ByteArray) = mac(key, sequenceOf(msg))
 fun MAC.mac(key: ByteArray, msg: Iterable<ByteArray>) = mac(key, msg.asSequence())
 
-private val HMAC.B get() = digest.inputBlockSize.bytes.toInt()
-private val HMAC.innerPad get() = ByteArray(B) { 0x36 }
-private val HMAC.outerPad get() = ByteArray(B) { 0x5C }
+private val HMAC.blockLength get() = digest.inputBlockSize.bytes.toInt()
+private val HMAC.innerPad get() = ByteArray(blockLength) { 0x36 }
+private val HMAC.outerPad get() = ByteArray(blockLength) { 0x5C }
 
 fun MAC.mac(key: ByteArray, msg: Sequence<ByteArray>): KmmResult<ByteArray> = catching {
     when (this@mac) {
@@ -22,10 +22,10 @@ fun MAC.mac(key: ByteArray, msg: Sequence<ByteArray>): KmmResult<ByteArray> = ca
 }
 
 internal fun HMAC.hmac(key: ByteArray, msg: Sequence<ByteArray>): ByteArray {
-    val realKey = (if (key.size <= B) key else digest.digest(key)).let {
-        if (it.size < B) it + ByteArray(B - it.size) else it
+    val realKey = (if (key.size <= blockLength) key else digest.digest(key)).let {
+        if (it.size < blockLength) it + ByteArray(blockLength - it.size) else it
     }
-    check(realKey.size == B)
+    check(realKey.size == blockLength)
     val innerHash = digest.digest(sequenceOf(realKey xor innerPad) + msg)
     val outerHash = digest.digest(sequenceOf(realKey xor outerPad, innerHash))
     return outerHash
--- supreme/src/jvmTest/kotlin/at/asitplus/signum/supreme/crypt/Test.kt
+++ supreme/src/jvmTest/kotlin/at/asitplus/signum/supreme/crypt/Test.kt
@@ -14,89 +14,84 @@ import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 import kotlin.random.Random
 
-@ExperimentalStdlibApi
 class JvmAESTest : FreeSpec({
 
     "Against JCA" - {
-        "Illegal IV Size" - {
+        withData(
+            SymmetricEncryptionAlgorithm.AES_128.CBC.PLAIN,
+            SymmetricEncryptionAlgorithm.AES_192.CBC.PLAIN,
+            SymmetricEncryptionAlgorithm.AES_256.CBC.PLAIN,
+
+            //JVM knows no AES-CBC-HMAC
+            SymmetricEncryptionAlgorithm.AES_128.GCM,
+            SymmetricEncryptionAlgorithm.AES_192.GCM,
+            SymmetricEncryptionAlgorithm.AES_256.GCM,
+
+            ) { alg ->
             withData(
-                SymmetricEncryptionAlgorithm.AES_128.CBC.PLAIN,
-                SymmetricEncryptionAlgorithm.AES_192.CBC.PLAIN,
-                SymmetricEncryptionAlgorithm.AES_256.CBC.PLAIN,
+                nameFn = { "iv: ${it?.size} bytes" }, alg.randomIV(), null
+            ) { iv ->
+                withData(Random.nextBytes(19), null) { aad ->
+                    withData(
+                        Random.nextBytes(19),
+                        Random.nextBytes(1),
+                        Random.nextBytes(0),
+                        Random.nextBytes(1234),
+                        Random.nextBytes(54),
+                        Random.nextBytes(16),
+                        Random.nextBytes(32),
+                        Random.nextBytes(256),
+                    ) { data ->
+                        val secretKey = alg.randomKey()
 
-                //JVM knows no AES-CBC-HMAC
+                        val jcaCipher =
+                            Cipher.getInstance(if (alg is SymmetricEncryptionAlgorithm.Unauthenticated) "AES/CBC/PKCS5PADDING" else "AES/GCM/NoPadding")
 
-                SymmetricEncryptionAlgorithm.AES_128.GCM,
-                SymmetricEncryptionAlgorithm.AES_192.GCM,
-                SymmetricEncryptionAlgorithm.AES_256.GCM,
+                        if (alg is SymmetricEncryptionAlgorithm.AES.GCM) {
+                            //GCM
+                            val own =
+                                alg.encryptorFor(secretKey, iv, aad).getOrThrow().encrypt(data)
+                                    .getOrThrow()
+                                    .shouldBeInstanceOf<Ciphertext.Authenticated>()
+                            jcaCipher.init(
+                                Cipher.ENCRYPT_MODE,
+                                SecretKeySpec(secretKey, "AES"),
+                                GCMParameterSpec(alg.tagNumBits.toInt(), own.iv/*use our own auto-generated IV*/)
+                            )
+                            if (aad != null) jcaCipher.updateAAD(aad)
 
-                ) { alg ->
+                            val encrypted = jcaCipher.doFinal(data)
 
-                withData(
-                    nameFn = { "iv: ${it?.size} bytes" }, alg.randomIV(), null
-                ) { iv ->
-                    withData(Random.nextBytes(19), null) { aad ->
-                        withData(
-                            Random.nextBytes(19),
-                            Random.nextBytes(1),
-                            Random.nextBytes(0),
-                            Random.nextBytes(1234),
-                            Random.nextBytes(54),
-                            Random.nextBytes(16),
-                            Random.nextBytes(32),
-                            Random.nextBytes(256),
-                        ) { data ->
-                            val secretKey = alg.randomKey()
+                            (own.encryptedData + own.authTag) shouldBe encrypted
 
-                            val jcaCipher =
-                                Cipher.getInstance(if (alg is SymmetricEncryptionAlgorithm.Unauthenticated) "AES/CBC/PKCS5PADDING" else "AES/GCM/NoPadding")
-
-                            if (alg is SymmetricEncryptionAlgorithm.AES.GCM) {
-                                //GCM
-                                val own =
-                                    alg.encryptorFor(secretKey, iv, aad).getOrThrow().encrypt(data)
-                                        .getOrThrow()
-                                        .shouldBeInstanceOf<Ciphertext.Authenticated>()
-                                jcaCipher.init(
-                                    Cipher.ENCRYPT_MODE,
-                                    SecretKeySpec(secretKey, "AES"),
-                                    GCMParameterSpec(alg.tagNumBits.toInt(), own.iv/*use our own auto-generated IV*/)
-                                )
-                                if (aad != null) jcaCipher.updateAAD(aad)
-
-                                val encrypted = jcaCipher.doFinal(data)
-
-                                (own.encryptedData + own.authTag) shouldBe encrypted
-
-                                jcaCipher.init(
-                                    Cipher.DECRYPT_MODE,
-                                    SecretKeySpec(secretKey, "AES"),
-                                    GCMParameterSpec(alg.tagNumBits.toInt(), own.iv/*use our own auto-generated IV*/)
-                                )
-                                if (aad != null) jcaCipher.updateAAD(aad)
-                                own.decrypt(secretKey).getOrThrow() shouldBe jcaCipher.doFinal(encrypted)
+                            jcaCipher.init(
+                                Cipher.DECRYPT_MODE,
+                                SecretKeySpec(secretKey, "AES"),
+                                GCMParameterSpec(alg.tagNumBits.toInt(), own.iv/*use our own auto-generated IV*/)
+                            )
+                            if (aad != null) jcaCipher.updateAAD(aad)
+                            own.decrypt(secretKey).getOrThrow() shouldBe jcaCipher.doFinal(encrypted)
 
 
-                            } else if (alg is SymmetricEncryptionAlgorithm.Unauthenticated) {
-                                //CBC
-                                val own = alg.encryptorFor(secretKey, iv).getOrThrow().encrypt(data).getOrThrow()
-                                jcaCipher.init(
-                                    Cipher.ENCRYPT_MODE,
-                                    SecretKeySpec(secretKey, "AES"),
-                                    IvParameterSpec(own.iv)/*use our own auto-generated IV, if null iv was provided*/
-                                )
-                                val encrypted = jcaCipher.doFinal(data)
+                        } else if (alg is SymmetricEncryptionAlgorithm.Unauthenticated) {
+                            //CBC
+                            val own = alg.encryptorFor(secretKey, iv).getOrThrow().encrypt(data).getOrThrow()
+                            jcaCipher.init(
+                                Cipher.ENCRYPT_MODE,
+                                SecretKeySpec(secretKey, "AES"),
+                                IvParameterSpec(own.iv)/*use our own auto-generated IV, if null iv was provided*/
+                            )
+                            val encrypted = jcaCipher.doFinal(data)
 
-                                own.encryptedData shouldBe encrypted
+                            own.encryptedData shouldBe encrypted
 
-                                jcaCipher.init(
-                                    Cipher.DECRYPT_MODE,
-                                    SecretKeySpec(secretKey, "AES"),
-                                    IvParameterSpec(own.iv)/*use our own auto-generated IV, if null iv was provided*/
-                                )
-                                own.decrypt(secretKey).getOrThrow() shouldBe jcaCipher.doFinal(encrypted)
+                            jcaCipher.init(
+                                Cipher.DECRYPT_MODE,
+                                SecretKeySpec(secretKey, "AES"),
+                                IvParameterSpec(own.iv)/*use our own auto-generated IV, if null iv was provided*/
+                            )
+                            own.decrypt(secretKey).getOrThrow() shouldBe jcaCipher.doFinal(encrypted)
 
-                            }
                         }
                     }
                 }
