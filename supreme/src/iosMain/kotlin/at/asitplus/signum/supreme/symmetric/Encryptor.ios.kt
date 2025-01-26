3acb7ac4d5819271ece48af71cfe69386b7c3419
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-01-26 19:17:13 +0100

    ios ECB mode

--- supreme/src/iosMain/kotlin/at/asitplus/signum/supreme/symmetric/Encryptor.ios.kt
+++ supreme/src/iosMain/kotlin/at/asitplus/signum/supreme/symmetric/Encryptor.ios.kt
@@ -13,11 +13,11 @@ actual internal fun <T, A : AuthType<out K>, I : Nonce, K : KeyType> initCipher(
     nonce: ByteArray?,
     aad: ByteArray?
 ): CipherParam<T, A, out K> {
-    if (algorithm.nonce !is Nonce.Required) TODO("ALGORITHM UNSUPPORTED")
-    algorithm as SymmetricEncryptionAlgorithm<A, Nonce.Required, K>
 
     @OptIn(HazardousMaterials::class)
-    val nonce = nonce ?: algorithm.randomNonce()
+    val nonce = if (algorithm.requiresNonce())
+        nonce ?: algorithm.randomNonce()
+    else null
     return CipherParam<ByteArray, AuthType<KeyType>, KeyType>(
         algorithm as SymmetricEncryptionAlgorithm<AuthType<KeyType>, Nonce.Required, KeyType>, key, nonce, aad
     ) as CipherParam<T, A, K>
@@ -26,14 +26,13 @@ actual internal fun <T, A : AuthType<out K>, I : Nonce, K : KeyType> initCipher(
 @OptIn(ExperimentalForeignApi::class)
 internal actual fun <A : AuthType<out K>, I : Nonce, K : KeyType> CipherParam<*, A, out K>.doEncrypt(data: ByteArray): SealedBox<A, I, out K> {
     this as CipherParam<ByteArray, A, K>
-    if (alg.nonce !is Nonce.Required) TODO("ALGORITHM UNSUPPORTED")
 
-    require(nonce != null)
+
 
 
     return when (alg) {
-        is AES<*, *> -> AESIOS.encrypt(alg, data, platformData, nonce, aad)
-        is SymmetricEncryptionAlgorithm.ChaCha20Poly1305 -> ChaChaIOS.encrypt(data, platformData, nonce, aad)
+        is AES<*, *, *> -> AESIOS.encrypt(alg, data, platformData, nonce, aad)
+        is SymmetricEncryptionAlgorithm.ChaCha20Poly1305 -> ChaChaIOS.encrypt(data, platformData, nonce!!, aad)
         else -> TODO("ALGORITHM UNSUPPORTED")
     } as SealedBox<A, I, K>
 }
@@ -46,7 +45,7 @@ internal actual fun SealedBox<Authenticated.Integrated, *, out KeyType.Integrate
     if (algorithm.nonce !is Nonce.Required) TODO("ALGORITHM UNSUPPORTED")
     this as SealedBox.WithNonce
     return when (algorithm) {
-        is AES<*, *> -> AESIOS.gcmDecrypt(encryptedData, secretKey, nonce, authTag, authenticatedData)
+        is AES<*, *, *> -> AESIOS.gcmDecrypt(encryptedData, secretKey, nonce, authTag, authenticatedData)
         is SymmetricEncryptionAlgorithm.ChaCha20Poly1305 -> ChaChaIOS.decrypt(
             encryptedData,
             secretKey,
@@ -63,10 +62,13 @@ internal actual fun SealedBox<Authenticated.Integrated, *, out KeyType.Integrate
 internal actual fun SealedBox<AuthType.Unauthenticated, *, out KeyType.Integrated>.doDecrypt(
     secretKey: ByteArray
 ): ByteArray {
-    if (algorithm.nonce !is Nonce.Required) TODO()
-    this as SealedBox.WithNonce
-    require(algorithm is AES<*, *>) { "Only AES is supported" }
+    require(algorithm is AES<*, *, *>) { "Only AES is supported" }
 
-    return AESIOS.cbcDecrypt(algorithm as AES<*, *>, encryptedData, secretKey, nonce)
+    return AESIOS.cbcEcbDecrypt(
+        algorithm as AES<*, *, *>,
+        encryptedData,
+        secretKey,
+        if (this is SealedBox.WithNonce) nonce else null
+    )
 
 }
