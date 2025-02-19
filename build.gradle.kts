5c3e6ddd464fe8f7b6188ba97c33c3a1870b8cb4
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-02-19 15:58:01 +0100

    update conventions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
 
 plugins {
-    id("at.asitplus.gradle.conventions") version "2.1.0+20250219"
+    id("at.asitplus.gradle.conventions") version "2.1.0+20250219+1"
     id("com.android.library") version "8.2.2" apply (false)
 }
 group = "at.asitplus.signum"
