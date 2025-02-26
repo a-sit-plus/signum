a258025f64f76d167eade71ab41cb6f1313fee14
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-02-26 14:57:21 +0100

    fix test case

--- supreme/src/jvmTest/kotlin/at/asitplus/signum/supreme/symmetric/Test.kt
+++ supreme/src/jvmTest/kotlin/at/asitplus/signum/supreme/symmetric/Test.kt
@@ -138,7 +138,7 @@ class JvmSymmetricTest : FreeSpec({
                                     alg.sealedBoxFrom(
                                         own.algorithm.randomNonce(),
                                         own.encryptedData
-                                    ).getOrThrow().decrypt(secretKey) shouldNot succeed
+                                    ).getOrThrow().decrypt(secretKey).onSuccess { it shouldNotBe data }
 
                             }
                         }
