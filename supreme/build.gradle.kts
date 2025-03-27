1f0192a9142aba0a5a2230737cb083331597edaf
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-03-27 17:13:29 +0100

    fix android instrumented tests

--- supreme/build.gradle.kts
+++ supreme/build.gradle.kts
@@ -107,6 +107,7 @@ android {
         androidTestImplementation(libs.core)
         androidTestImplementation(libs.rules)
         androidTestImplementation(libs.kotest.runner.android)
+        androidTestImplementation("at.asitplus:kmmresult-test:${AspVersions.kmmresult}")
         testImplementation(libs.kotest.extensions.android)
     }
 
