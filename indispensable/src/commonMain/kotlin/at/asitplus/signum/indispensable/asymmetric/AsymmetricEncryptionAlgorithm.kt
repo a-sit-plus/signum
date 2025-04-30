ed37d8d0f1039244c771ed9bbbc30c7c30e7cc09
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-04-30 09:37:41 +0200

    RSA encryption tests from OpenSSL

--- indispensable/src/commonMain/kotlin/at/asitplus/signum/indispensable/asymmetric/AsymmetricEncryptionAlgorithm.kt
+++ indispensable/src/commonMain/kotlin/at/asitplus/signum/indispensable/asymmetric/AsymmetricEncryptionAlgorithm.kt
@@ -2,19 +2,40 @@ package at.asitplus.signum.indispensable.asymmetric
 
 import at.asitplus.signum.HazardousMaterials
 import at.asitplus.signum.indispensable.Digest
+import at.asitplus.signum.indispensable.SecretExposure
 
 
 sealed interface RSAPadding {
-    object PKCS1: RSAPadding
-    @HazardousMaterials("This is almost always insecure and can leak your private key!")
-    object NONE: RSAPadding
-
-    sealed class OAEP(val digest: Digest): RSAPadding{
-        object SHA1: OAEP(Digest.SHA1)
-        object SHA256: OAEP(Digest.SHA256)
-        object SHA384: OAEP(Digest.SHA384)
-        object SHA512: OAEP(Digest.SHA512)
+    @HazardousMaterials("This padding Scheme is vulnerable to Bleichenbacher's attack. Use only with legacy application where you absolutely must")
+    object PKCS1 : RSAPadding {
+        override fun toString(): String = "PKCS1"
     }
+
+    @HazardousMaterials("This is almost always insecure and can leak your private key!")
+    object NONE : RSAPadding {
+        override fun toString(): String = "NONE"
+    }
+
+
+    sealed class OAEP(val digest: Digest) : RSAPadding {
+        object SHA1 : OAEP(Digest.SHA1)
+        object SHA256 : OAEP(Digest.SHA256)
+        object SHA384 : OAEP(Digest.SHA384)
+        object SHA512 : OAEP(Digest.SHA512)
+
+        override fun toString(): String = "OAEP_${digest.name}"
+    }
+
+    companion object {
+        val entries: List<RSAPadding> by lazy {
+            @OptIn(HazardousMaterials::class)
+            listOf(PKCS1, NONE, OAEP.SHA1, OAEP.SHA256, OAEP.SHA384, OAEP.SHA512)
+        }
+
+        fun fromString(string: String) = entries.firstOrNull { it.toString() == string }
+    }
+
+
 }
 
 sealed interface AsymmetricEncryptionAlgorithm {
--- supreme/src/commonTest/kotlin/at/asitplus/signum/supreme/asymmetric/0000RsaEncrpytionTest.kt
+++ supreme/src/commonTest/kotlin/at/asitplus/signum/supreme/asymmetric/0000RsaEncrpytionTest.kt
@@ -6,31 +6,93 @@ import at.asitplus.signum.indispensable.SecretExposure
 import at.asitplus.signum.indispensable.asn1.encodeToPEM
 import at.asitplus.signum.indispensable.asymmetric.AsymmetricEncryptionAlgorithm
 import at.asitplus.signum.indispensable.asymmetric.RSAPadding
-import at.asitplus.signum.supreme.sign.EphemeralKey
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.datatest.withData
-import at.asitplus.signum.supreme.asymmetric.decryptorFor
+import io.kotest.matchers.shouldBe
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.Json
 
 @OptIn(HazardousMaterials::class, SecretExposure::class, ExperimentalStdlibApi::class)
 class `0000RsaEncrpytionTest` : FreeSpec({
-    "Basic" - {
-        val k = EphemeralKey {
-            rsa {
-                bits = 2048
-            }
-        }.getOrThrow()
 
-        val pub = k.publicKey
-        val data = "1337".encodeToByteArray()
-        withData(RSAPadding.PKCS1, RSAPadding.NONE, RSAPadding.OAEP.SHA256, RSAPadding.OAEP.SHA384, RSAPadding.OAEP.SHA512) {
-            println(k.exportPrivateKey().getOrThrow().encodeToPEM().getOrThrow())
-            val encrypted = AsymmetricEncryptionAlgorithm.RSA(it).encryptorFor(pub).encrypt(data).getOrThrow()
-            println(encrypted.toHexString())
-            println()
-           val decrypted = AsymmetricEncryptionAlgorithm.RSA(it).decryptorFor(k.exportPrivateKey().getOrThrow() as CryptoPrivateKey.RSA).decrypt(encrypted).getOrThrow()
-            println(decrypted.decodeToString())
-            println()
-            println()
+    "From OpenSSL" - {
+        withData(nameFn = { it.toString() }, testData) {
+            it.key as CryptoPrivateKey.RSA
+            AsymmetricEncryptionAlgorithm.RSA(it.padding).decryptorFor(it.key).decrypt(it.enc)
+                .getOrThrow() shouldBe it.plain
+            val newEncrypted =
+                AsymmetricEncryptionAlgorithm.RSA(it.padding).encryptorFor(it.key.publicKey).encrypt(it.plain)
+                    .getOrThrow()
+
+            AsymmetricEncryptionAlgorithm.RSA(it.padding).decryptorFor(it.key).decrypt(newEncrypted)
+                .getOrThrow() shouldBe it.plain
         }
     }
 })
+
+@Serializable
+class RsaTestData(
+    @Serializable(PemSerializer::class) val key: CryptoPrivateKey,
+    @Serializable(ByteArrayHexSerializer::class) val plain: ByteArray,
+    @Serializable(ByteArrayHexSerializer::class) val enc: ByteArray,
+    @Serializable(PaddingSerializer::class) val padding: RSAPadding
+) {
+
+    @OptIn(ExperimentalStdlibApi::class)
+    override fun toString() = "$padding, Data=${plain.toHexString(HexFormat.Default)}, Encrypted=${enc.toHexString()}"
+
+    companion object {
+        object PemSerializer : KSerializer<CryptoPrivateKey> {
+            override val descriptor: SerialDescriptor =
+                PrimitiveSerialDescriptor("PemEncodedPrivateKey", PrimitiveKind.STRING)
+
+            override fun serialize(
+                encoder: Encoder,
+                value: CryptoPrivateKey
+            ) {
+                encoder.encodeString(value.encodeToPEM().getOrThrow())
+            }
+
+            override fun deserialize(decoder: Decoder) =
+                CryptoPrivateKey.decodeFromPem(decoder.decodeString()).getOrThrow()
+        }
+
+        object PaddingSerializer : KSerializer<RSAPadding> {
+            override val descriptor: SerialDescriptor =
+                PrimitiveSerialDescriptor("Padding", PrimitiveKind.STRING)
+
+            override fun deserialize(decoder: Decoder) =
+                decoder.decodeString().let { decoded -> RSAPadding.entries.first { it.toString() == decoded } }
+
+            override fun serialize(
+                encoder: Encoder,
+                value: RSAPadding
+            ) {
+                encoder.encodeString(value.toString())
+            }
+        }
+
+        object ByteArrayHexSerializer : KSerializer<ByteArray> {
+            override val descriptor: SerialDescriptor =
+                PrimitiveSerialDescriptor("ByteArrayHex", PrimitiveKind.STRING)
+
+            @OptIn(ExperimentalStdlibApi::class)
+            override fun deserialize(decoder: Decoder) = decoder.decodeString().hexToByteArray()
+
+            @OptIn(ExperimentalStdlibApi::class)
+            override fun serialize(encoder: Encoder, value: ByteArray) {
+                encoder.encodeString(value.toHexString())
+            }
+        }
+    }
+
+}
+
+
+private val testData = Json.decodeFromString<List<RsaTestData>>(rsaInputString)
