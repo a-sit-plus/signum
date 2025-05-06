14c0489e43fe570a99d873ecdd15e16ec0a031b7
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-05-06 23:19:00 +0200

    fixes

--- indispensable-asn1/src/jvmTest/kotlin/at/asitplus/signum/indispensable/asn1/RealTest.kt
+++ indispensable-asn1/src/jvmTest/kotlin/at/asitplus/signum/indispensable/asn1/RealTest.kt
@@ -43,7 +43,8 @@ class RealTest : FreeSpec({
 
     "Special values" - {
         "1.1897314953572317650857593266280070162123456789009876543456789098765432123456789876543212345678987654323456789876532345678765432345678876543234567" {
-            val number = this.testCase.name.name
+            val number = this.testCase.name.name.let { it.substring(0 ..< it.indexOf("[")) }
+            println("NUMBER: $number")
             val bigDecimal = BigDecimal.parseString(number)
             bigDecimal.precision shouldBeGreaterThan 64L
             val wrongScaledMantissa = bigDecimal.significand.toAsn1Integer()
