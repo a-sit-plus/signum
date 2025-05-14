5fbc57f9aab1bc4698f646d2b4b7cdee314083f2
Srdjan Stjepanovic <srdjan.stjepanovic@iaik.tugraz.at>
2025-05-14 11:01:44 +0200

    fix bugs in nameConstraints asn1 classes and finish nameConstraints validator

--- supreme/src/commonMain/kotlin/at/asitplus/signum/supreme/validate/NameConstraintsValidator.kt
+++ supreme/src/commonMain/kotlin/at/asitplus/signum/supreme/validate/NameConstraintsValidator.kt
@@ -1,4 +1,54 @@
 package at.asitplus.signum.supreme.validate
 
-class NameConstraintsValidator {
+import at.asitplus.signum.CertificateChainValidatorException
+import at.asitplus.signum.NameConstraintsException
+import at.asitplus.signum.indispensable.asn1.KnownOIDs
+import at.asitplus.signum.indispensable.pki.X509Certificate
+import at.asitplus.signum.indispensable.pki.pkiExtensions.NameConstraints
+import at.asitplus.signum.indispensable.pki.pkiExtensions.decodeNameConstraints
+import kotlinx.io.IOException
+
+class NameConstraintsValidator(
+    private val pathLength: Int,
+    private val currentCertIndex: Int = 0,
+    private var previousNameConstraints: NameConstraints? = null
+) {
+
+    fun checkNameConstraints(currCert: X509Certificate) {
+        if (previousNameConstraints != null && (currentCertIndex == pathLength || !currCert.isSelfIssued())) {
+
+            try {
+                if (!previousNameConstraints!!.verify(currCert)) {
+                    throw NameConstraintsException("")
+                }
+            } catch (e: IOException) {
+                throw CertificateChainValidatorException(
+                    e.message ?: "NameConstraints validation failed."
+                )
+            }
+        }
+
+        previousNameConstraints = mergeNameConstraints(currCert, previousNameConstraints)
+    }
+
+    private fun mergeNameConstraints(
+        currCert: X509Certificate,
+        previousNameConstraints: NameConstraints?
+    ): NameConstraints? {
+
+        val newNameConstraints =
+            currCert.findExtension(KnownOIDs.nameConstraints_2_5_29_30)?.decodeNameConstraints()
+
+        return if (previousNameConstraints == null) {
+            newNameConstraints?.copy()
+        } else {
+            try {
+                previousNameConstraints.merge(newNameConstraints)
+            } catch (ioe: IOException) {
+                throw NameConstraintsException(ioe.message ?: "NameConstraints merge failed.")
+            }
+            previousNameConstraints
+        }
+    }
+
 }
