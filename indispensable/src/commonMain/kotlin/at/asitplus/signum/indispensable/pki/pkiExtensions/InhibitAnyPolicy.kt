4094ee6ee92e43e2e31d32c783810476d8355018
Srdjan Stjepanovic <srdjan.stjepanovic@a-sit.at>
2025-05-14 10:50:50 +0200

    add X500Name and refactor X509Cert to use this class as subject and issuer

--- indispensable/src/commonMain/kotlin/at/asitplus/signum/indispensable/pki/pkiExtensions/InhibitAnyPolicy.kt
+++ indispensable/src/commonMain/kotlin/at/asitplus/signum/indispensable/pki/pkiExtensions/InhibitAnyPolicy.kt
@@ -1,4 +1,4 @@
-package at.asitplus.signum.supreme.validate.pkiExtensions
+package at.asitplus.signum.indispensable.pki.pkiExtensions
 
 import at.asitplus.signum.indispensable.asn1.Asn1Element
 import at.asitplus.signum.indispensable.asn1.Asn1Integer
