3ab0296e092b29a2bd00496b80635f92f6778361
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-06-23 17:41:37 +0200

    implicit deserialization progress

--- indispensable-asn1/src/jvmTest/kotlin/at/asitplus/signum/indispensable/asn1/serialization/SerializationTest.kt
+++ indispensable-asn1/src/jvmTest/kotlin/at/asitplus/signum/indispensable/asn1/serialization/SerializationTest.kt
@@ -1,6 +1,8 @@
 package at.asitplus.signum.indispensable.asn1.serialization
 
 import at.asitplus.signum.indispensable.asn1.Asn1OctetString
+import at.asitplus.signum.indispensable.asn1.Asn1TagMismatchException
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FreeSpec
 import kotlinx.serialization.Serializable
 import kotlin.random.Random
@@ -8,6 +10,42 @@ import kotlin.random.Random
 
 @OptIn(ExperimentalStdlibApi::class)
 class SerializationTest : FreeSpec({
+
+    "Implicit tagging" {
+        val imlNothing = encodeToDer(NothingOnClass("foo"))
+        val imlClass = encodeToDer(ImplicitOnClass("foo"))
+        val imlProp = encodeToDer(ImplicitOnProperty("foo"))
+        val imlBoth = encodeToDer(ImplicitOnBoth("foo"))
+
+        shouldThrow<Asn1TagMismatchException> { decodeFromDer<ImplicitOnProperty>(imlClass) }
+        shouldThrow<Asn1TagMismatchException> { decodeFromDer<ImplicitOnProperty>(imlBoth) }
+        shouldThrow<Asn1TagMismatchException> { decodeFromDer<ImplicitOnProperty>(imlNothing) }
+
+        shouldThrow<Asn1TagMismatchException> { decodeFromDer<ImplicitOnClass>(imlNothing) }
+        shouldThrow<Asn1TagMismatchException> { decodeFromDer<ImplicitOnClass>(imlBoth) }
+        shouldThrow<Asn1TagMismatchException> { decodeFromDer<ImplicitOnClass>(imlProp) }
+
+        shouldThrow<Asn1TagMismatchException> { decodeFromDer<ImplicitOnBoth>(imlProp) }
+        shouldThrow<Asn1TagMismatchException> { decodeFromDer<ImplicitOnBoth>(imlClass) }
+        shouldThrow<Asn1TagMismatchException> { decodeFromDer<ImplicitOnBoth>(imlNothing) }
+
+        shouldThrow<Asn1TagMismatchException> { decodeFromDer<NothingOnClass>(imlClass) }
+        shouldThrow<Asn1TagMismatchException> { decodeFromDer<NothingOnClass>(imlProp) }
+        shouldThrow<Asn1TagMismatchException> { decodeFromDer<NothingOnClass>(imlBoth) }
+
+
+        shouldThrow<Asn1TagMismatchException> { decodeFromDer<ImplicitOnClassWrong>(imlClass) }
+        shouldThrow<Asn1TagMismatchException> { decodeFromDer<ImplicitOnPropertyWrong>(imlProp) }
+        shouldThrow<Asn1TagMismatchException> { decodeFromDer<ImplicitOnBothWrong>(imlBoth) }
+        shouldThrow<Asn1TagMismatchException> { decodeFromDer<ImplicitOnBothWrongClass>(imlBoth) }
+        shouldThrow<Asn1TagMismatchException> { decodeFromDer<ImplicitOnBothWrongProperty>(imlBoth) }
+
+        decodeFromDer<NothingOnClass>(imlNothing)
+        decodeFromDer<ImplicitOnClass>(imlClass)
+        decodeFromDer<ImplicitOnProperty>(imlProp)
+        decodeFromDer<ImplicitOnBoth>(imlBoth)
+
+    }
 
     "Writing" {
 
@@ -38,7 +76,7 @@ class SerializationTest : FreeSpec({
                 octet = Asn1OctetString("Hello World".encodeToByteArray())
             )
         )
-        println(            derEncoded.toHexString()        )
+        println(derEncoded.toHexString())
 
         val string = "Foo"
         println(encodeToDer(string).toHexString())
@@ -85,9 +123,9 @@ data class TypesUmbrella(
 
     //@Asn1OctetString
     val inner: Simple,
-   // @Asn1ImplicitlyTagged(333uL)
+    // @Asn1ImplicitlyTagged(333uL)
     val str: String,
-  //  @Asn1OctetString
+    //  @Asn1OctetString
     val i: UInt,
     @Asn1EncodeNull
     val nullable: Double?,
@@ -168,3 +206,36 @@ data class NullableByteString(
         return byteString?.contentHashCode() ?: 0
     }
 }
+
+@Serializable
+class NothingOnClass(val a: String)
+
+@Serializable
+@Asn1ImplicitlyTagged(1337uL)
+class ImplicitOnClass(val a: String)
+
+@Serializable
+@Asn1ImplicitlyTagged(3771uL)
+class ImplicitOnClassWrong(val a: String)
+
+@Serializable
+class ImplicitOnProperty(@Asn1ImplicitlyTagged(1338uL) val a: String)
+
+@Serializable
+class ImplicitOnPropertyWrong(@Asn1ImplicitlyTagged(8331uL) val a: String)
+
+@Serializable
+@Asn1ImplicitlyTagged(1337uL)
+class ImplicitOnBoth(@Asn1ImplicitlyTagged(1338uL) val a: String)
+
+@Serializable
+@Asn1ImplicitlyTagged(7331uL)
+class ImplicitOnBothWrong(@Asn1ImplicitlyTagged(8331uL) val a: String)
+
+@Serializable
+@Asn1ImplicitlyTagged(7331uL)
+class ImplicitOnBothWrongClass(@Asn1ImplicitlyTagged(1338uL) val a: String)
+
+@Serializable
+@Asn1ImplicitlyTagged(1337uL)
+class ImplicitOnBothWrongProperty(@Asn1ImplicitlyTagged(8331uL) val a: String)
