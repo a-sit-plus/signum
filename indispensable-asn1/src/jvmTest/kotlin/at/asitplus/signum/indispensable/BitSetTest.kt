d1dfeadf6fd3fc9fe2a8d2f35368e5cb7e3494fe
Srdjan Stjepanovic <srdjan.stjepanovic@a-sit.at>
2025-06-23 12:38:29 +0200

    address code style review suggestions

--- indispensable-asn1/src/jvmTest/kotlin/at/asitplus/signum/indispensable/BitSetTest.kt
+++ indispensable-asn1/src/jvmTest/kotlin/at/asitplus/signum/indispensable/BitSetTest.kt
@@ -161,7 +161,7 @@ class BitSetTest : FreeSpec({
 
                 input.forEachIndexed { i, b ->
                     withClue("jvm[$i]") { jvm[i] shouldBe b }
-                    withClue("kmm[$i]") { kmm.get(i.toLong()) shouldBe b }
+                    withClue("kmm[$i]") { kmm[i.toLong()] shouldBe b }
                 }
 
                 withClue("first bit set") { kmm.nextSetBit(0).toInt() shouldBe jvm.nextSetBit(0) }
@@ -235,7 +235,7 @@ class BitSetTest : FreeSpec({
 
                     input.forEachIndexed { i, b ->
                         withClue("jvm[$i]") { jvm[i] shouldBe b }
-                        withClue("kmm[$i]") { kmm.get(i.toLong()) shouldBe b }
+                        withClue("kmm[$i]") { kmm[i.toLong()] shouldBe b }
                     }
 
                     val truncated = input.dropLastWhile { !it }
