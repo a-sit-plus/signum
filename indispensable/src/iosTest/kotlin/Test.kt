ddd02eaef58062d2cd5c5161626a5c30b9e88563
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-06-29 11:07:31 +0200

    proof

--- indispensable/src/iosTest/kotlin/Test.kt
+++ indispensable/src/iosTest/kotlin/Test.kt
@@ -1,9 +1,9 @@
 import io.kotest.core.spec.style.FreeSpec
-import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.shouldBe
 
 class Test : FreeSpec({
 
     "This dummy test" {
-        "is just making sure" shouldNotBe "that iOS tests are indeed running"
+        "is just making sure" shouldBe "that iOS tests are indeed running"
     }
 })
