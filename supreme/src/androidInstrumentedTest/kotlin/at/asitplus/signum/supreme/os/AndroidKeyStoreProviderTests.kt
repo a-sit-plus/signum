aed81717d12549ee69f8bfa90da7f8d5c60536e6
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-06-30 20:25:59 +0200

    downgrade to kotest 6.0.0.M1

--- supreme/src/androidInstrumentedTest/kotlin/at/asitplus/signum/supreme/os/AndroidKeyStoreProviderTests.kt
+++ supreme/src/androidInstrumentedTest/kotlin/at/asitplus/signum/supreme/os/AndroidKeyStoreProviderTests.kt
@@ -8,18 +8,14 @@ import at.asitplus.signum.supreme.signature
 import at.asitplus.signum.supreme.succeed
 import br.com.colman.kotest.FreeSpec
 import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
-import io.kotest.property.Arb
-import io.kotest.property.RandomSource
-import io.kotest.property.arbitrary.Codepoint
-import io.kotest.property.arbitrary.az
-import io.kotest.property.arbitrary.string
+import io.kotest.property.azstring
 import kotlin.random.Random
 
 class AndroidKeyStoreProviderTests : FreeSpec({
     "Create attested keypair" {
-        val alias = Arb.string(minSize = 32, maxSize = 32, Codepoint.az())
-            .sample(RandomSource.default()).value
+        val alias = Random.azstring(32)
         val attestChallenge = Random.nextBytes(32)
         val hardwareSigner = AndroidKeyStoreProvider.createSigningKey(alias) {
             hardware {
