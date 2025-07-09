760d52903295c7603044ef6339e858347613767d
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-07-09 18:20:41 +0200

    refactor oid descrptions

--- indispensable-oids/src/commonMain/kotlin/OidExtensions.kt
+++ indispensable-oids/src/commonMain/kotlin/OidExtensions.kt
@@ -1,19 +1,11 @@
 package at.asitplus.signum.indispensable.asn1
 
-import at.asitplus.signum.indispensable.asn1.OidMap.lookupDescription
-
-private val customOidDescriptions = mutableMapOf<ObjectIdentifier, String>()
-
 /**
- * Adds or overrides a description of an Object Identifier. This is useful for communicating context to humans, especially for (but not limited to) debugging.
- * Most well-known OIDs are already described in [OidMap].
- * This method is neither thread-safe not coroutine-safe! Unguarded concurrent calls can cause loss of descriptions.
- * OID descriptions need to live outside the actual OID objects, because this semantic enhancement will never be serialized and thus cannot be deserialized.
+ * Ensures that known OIDs have their description attached. Call this method if you want human-readable OIDs for debugging.
+ *
+ * The first call to this function triggers the initialization process of all [KnownOIDs] descriptions.
+ * Future calls to this function are a NOOP.
  */
-fun ObjectIdentifier.describe(description: String) = apply { customOidDescriptions[this] = description }
-
-/**
- * Returns a human-readable description of this OID. Virtually all commonly used OIDs will have this method pull a description from [KnownOIDs]/[OidMap].
- * An OID's description can be set/overridden using [describe]
- */
-val ObjectIdentifier.description: String? get() = customOidDescriptions[this] ?: lookupDescription()
+fun ObjectIdentifier.Companion.describeKnownOIDs() {
+    OidMap.initDescriptions()
+}
