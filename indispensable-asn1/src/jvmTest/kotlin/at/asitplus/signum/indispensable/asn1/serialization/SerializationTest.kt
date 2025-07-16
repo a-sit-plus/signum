62206439e606d0ec3b933b7f8d2f04d1a3a9e689
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-07-16 22:47:45 +0200

    fail hard: needs separate indexes for elements and descriptors to handle nullables

--- indispensable-asn1/src/jvmTest/kotlin/at/asitplus/signum/indispensable/asn1/serialization/SerializationTest.kt
+++ indispensable-asn1/src/jvmTest/kotlin/at/asitplus/signum/indispensable/asn1/serialization/SerializationTest.kt
@@ -22,6 +22,80 @@ import kotlin.random.Random
 class SerializationTest : FreeSpec({
 
 
+    "Number of elements" {
+
+        val withThreeOtherNull = WithThreeNullable("first", null, "3")
+
+        DER.encodeToDer(withThreeOtherNull).apply {
+            toHexString() shouldBe "300a0c0566697273740c0133"
+            DER.decodeFromDer<WithThreeNullable>(this) shouldBe withThreeOtherNull
+        }
+
+
+        val withThreeOther = WithThreeNullable("first", 2, "3")
+
+        DER.encodeToDer(withThreeOther).apply {
+            toHexString() shouldBe "300d0c0566697273740201020c0133"
+            DER.decodeFromDer<WithThreeNullable>(this) shouldBe withThreeOther
+        }
+
+        val without = Without
+        val withOne = WithOne("")
+        val withTwo = WithTwo("1", "2")
+        val withTwoOther = WithTwoOther("1", 2)
+        val withThree = WithThree("1", "3", "3")
+
+        DER.encodeToDer(without).apply {
+            toHexString() shouldBe "3000"
+            DER.decodeFromDer<Without>(this) shouldBe without
+        }
+
+        shouldThrow<SerializationException> { DER.decodeFromDer<Without>("30020c00".hexToByteArray()) }
+
+        DER.encodeToDer(withOne).apply {
+            toHexString() shouldBe "30020c00"
+            DER.decodeFromDer<WithOne>(this) shouldBe withOne
+        }
+
+        DER.encodeToDer(withTwo).apply {
+            toHexString() shouldBe "30060c01310c0132"
+            DER.decodeFromDer<WithTwo>(this) shouldBe withTwo
+        }
+
+        DER.encodeToDer(withTwoOther).apply {
+            toHexString() shouldBe "30060c0131020102"
+            DER.decodeFromDer<WithTwoOther>(this) shouldBe withTwoOther
+        }
+
+        DER.encodeToDer(withThree).apply {
+            toHexString() shouldBe "30090c01310c01330c0133"
+            DER.decodeFromDer<WithThree>(this) shouldBe withThree
+        }
+    }
+
+    "Polymorphic" {
+        val without = Without
+        val withOne = WithOne("")
+        val withTwo = WithTwo("1", "2")
+        val withTwoOther = WithTwoOther("1", 2)
+        val withThree = WithThree("1", "3", "3")
+
+        DER.encodeToDer(listOf(without, withOne, withTwo, withTwoOther, withThree)).apply { println(toHexString()) }
+
+        DER.decodeFromDer<List<AnInterface>>("3082017730430c3f61742e61736974706c75732e7369676e756d2e696e64697370656e7361626c652e61736e312e73657269616c697a6174696f6e2e6170692e576974686f7574300030450c3f61742e61736974706c75732e7369676e756d2e696e64697370656e7361626c652e61736e312e73657269616c697a6174696f6e2e6170692e576974684f6e6530020c0030490c3f61742e61736974706c75732e7369676e756d2e696e64697370656e7361626c652e61736e312e73657269616c697a6174696f6e2e6170692e5769746854776f30060c01310c0132304e0c4461742e61736974706c75732e7369676e756d2e696e64697370656e7361626c652e61736e312e73657269616c697a6174696f6e2e6170692e5769746854776f4f7468657230060c0131020102304e0c4161742e61736974706c75732e7369676e756d2e696e64697370656e7361626c652e61736e312e73657269616c697a6174696f6e2e6170692e57697468546872656530090c01310c01330c0133".hexToByteArray())
+
+    }
+
+
+    "SET semantics" {
+        val set = setOf("Foo", "Bar", "Baz")
+        DER.decodeFromDer<Set<String>>(
+            DER.encodeToDer(set).also { it.toHexString() shouldBe "310f0c03466f6f0c034261720c0342617a" }) shouldBe set
+        DER.decodeFromDer<Set<String>>(
+            DER.encodeToDer(set).also { it.toHexString() shouldBe "310f0c03466f6f0c034261720c0342617a" }) shouldBe set
+    }
+
+
     "Nulls and Noughts" {
 
 
@@ -52,21 +126,12 @@ class SerializationTest : FreeSpec({
         DER.decodeFromDer<NullableAnnotatedImplicitOmit?>(byteArrayOf()) shouldBe null
 
 
-
         val annotated = DER.encodeToDer<NullableAnnotated?>(null)
             .apply { toHexString(HexFormat.UpperCase) shouldBe "0406BF8A39020500" }
         DER.decodeFromDer<NullableAnnotated?>(annotated) shouldBe null
 
 
     }
-
-
-    "SET semantics" {
-        val set = setOf("Foo", "Bar", "Baz")
-        DER.decodeFromDer<Set<String>>(
-            DER.encodeToDer(set).also { it.toHexString() shouldBe "310f0c03466f6f0c034261720c0342617a" }) shouldBe set
-    }
-
 
     "Implicit tagging" - {
         val imlNothing = DER.encodeToDer(NothingOnClass("foo")).also { println("imlNothing " + it.toHexString()) }
@@ -254,7 +319,6 @@ class SerializationTest : FreeSpec({
             DER.decodeFromDer<BitSetValueAnnotated>(
                 DER.encodeToDer(valueClassEmptyAnnotated)
                     .also { it.toHexString() shouldBe "040bbf8a3b07bf8a3903030100" }).bytes shouldBe valueClassEmptyAnnotated.bytes
-
 
 
             val normalEmpty = BitSetNormal(empty)
@@ -879,3 +943,16 @@ data class InternalNullableAnnotatedOmit(
     )
     val nullable: String?
 )
+
+@Serializable
+sealed interface AnInterface
+
+// @formatter:off
+@Serializable object Without : AnInterface
+@Serializable data class WithOne(val first:String) : AnInterface
+@Serializable data class WithTwo(val first:String, val second:String) : AnInterface
+@Serializable data class WithTwoOther(val first:String, val second:Int) : AnInterface
+@Serializable data class WithThree(val first:String, val second:String, val third:String) : AnInterface
+
+@Serializable data class WithThreeNullable(val first:String, val second:Int?, val third:String)
+// @formatter:on
