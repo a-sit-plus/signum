7daf6c54bca77873d4823a692dc613647bf425e2
Srdjan Stjepanovic <srdjan.stjepanovic@a-sit.at>
2025-07-22 09:09:50 +0200

    refactor decoding after rebase

--- indispensable/src/commonMain/kotlin/at/asitplus/signum/indispensable/pki/OCSPResponse.kt
+++ indispensable/src/commonMain/kotlin/at/asitplus/signum/indispensable/pki/OCSPResponse.kt
@@ -16,6 +16,7 @@ import at.asitplus.signum.indispensable.asn1.KnownOIDs
 import at.asitplus.signum.indispensable.asn1.ObjectIdentifier
 import at.asitplus.signum.indispensable.asn1.PemDecodable
 import at.asitplus.signum.indispensable.asn1.PemEncodable
+import at.asitplus.signum.indispensable.asn1.decodeRethrowing
 import at.asitplus.signum.indispensable.asn1.encoding.Asn1
 import at.asitplus.signum.indispensable.asn1.encoding.decode
 import at.asitplus.signum.indispensable.asn1.encoding.decodeFromAsn1ContentBytes
@@ -24,6 +25,7 @@ import at.asitplus.signum.indispensable.asn1.encoding.encodeToAsn1ContentBytes
 import at.asitplus.signum.indispensable.asn1.encoding.parse
 import io.matthewnelson.encoding.base64.Base64
 import io.matthewnelson.encoding.core.Decoder.Companion.decodeToByteArray
+import at.asitplus.signum.indispensable.asn1.*
 
 
 data class RevokedInfo(
@@ -38,11 +40,11 @@ data class RevokedInfo(
 
     companion object : Asn1Decodable<Asn1ExplicitlyTagged, RevokedInfo> {
 
-        override fun doDecode(src: Asn1ExplicitlyTagged): RevokedInfo {
+        override fun doDecode(src: Asn1ExplicitlyTagged): RevokedInfo = src.decodeRethrowing {
             src.verifyTag(SingleResponse.CertStatus.REVOKED.tag)
-            val time = Asn1Time.decodeFromTlv(src.nextChild().asPrimitive())
-            val reason = if (src.hasMoreChildren()) {
-                src.nextChild()
+            val time = Asn1Time.decodeFromTlv(next().asPrimitive())
+            val reason = if (hasNext()) {
+                next()
             } else null
             return RevokedInfo(time, reason)
         }
@@ -102,19 +104,19 @@ data class SingleResponse(
             val EXTENSIONS = Asn1.ExplicitTag(1uL)
         }
 
-        override fun doDecode(src: Asn1Sequence): SingleResponse {
-            val certId = CertId.decodeFromTlv(src.nextChild().asSequence())
-            val data = src.nextChild()
+        override fun doDecode(src: Asn1Sequence): SingleResponse = src.decodeRethrowing {
+            val certId = CertId.decodeFromTlv(next().asSequence())
+            val data = next()
             val status = CertStatus.fromTag(data.tag.tagValue)
 
-            val update = Asn1Time.decodeFromTlv(src.nextChild().asPrimitive())
+            val update = Asn1Time.decodeFromTlv(next().asPrimitive())
 
-            val nextUpdate = if (src.hasMoreChildren() && src.peek()?.tag == Tags.NEXT_UPDATE) {
-                Asn1Time.decodeFromTlv(src.nextChild().asExplicitlyTagged().nextChild().asPrimitive())
+            val nextUpdate = if (hasNext() && peek()?.tag == Tags.NEXT_UPDATE) {
+                Asn1Time.decodeFromTlv(next().asExplicitlyTagged().decodeRethrowing { next().asPrimitive() })
             } else null
 
-            val extensions = if (src.hasMoreChildren()) {
-                (src.nextChild().asExplicitlyTagged().verifyTag(Tags.EXTENSIONS.tagValue)
+            val extensions = if (hasNext()) {
+                (next().asExplicitlyTagged().verifyTag(Tags.EXTENSIONS.tagValue)
                     .single().asSequence()).children.map {
                         X509CertificateExtension.decodeFromTlv(it.asSequence())
                     }
@@ -132,33 +134,81 @@ data class SingleResponse(
     }
 }
 
-data class ResponseData(
-    val version: Int? = 2,
+data class ResponderID(
     val byName: List<RelativeDistinguishedName>? = null,
-    val byKey: ByteArray? = null,
-    val producedAt: Asn1Time,
-    val responses: List<SingleResponse>,
-    val responsesExtensions: List<X509CertificateExtension>? = null
-) : Asn1Encodable<Asn1Sequence> {
+    val byKey: ByteArray? = null
+) : Asn1Encodable<Asn1ExplicitlyTagged> {
 
     init {
         if (byName == null && byKey == null) throw Asn1StructuralException("Invalid ResponderID in Response Data.")
     }
 
-    override fun encodeToTlv(): Asn1Sequence = Asn1.Sequence{
-        version?.let { Asn1.ExplicitlyTagged(Tags.VERSION.tagValue) { +Asn1.Int(version) } }
-        byName?.let {
-            +Asn1.ExplicitlyTagged(1uL) {
+    override fun encodeToTlv(): Asn1ExplicitlyTagged {
+        return when {
+            byName != null -> Asn1.ExplicitlyTagged(1uL) {
                 +Asn1.Sequence {
                     byName.forEach { +it }
                 }
             }
-        }
-        byKey?.let {
-            +Asn1.ExplicitlyTagged(2uL) {
+            byKey != null -> Asn1.ExplicitlyTagged(2uL) {
                 +Asn1Primitive(Asn1Element.Tag.OCTET_STRING, byKey)
             }
+            else -> throw IllegalStateException("Either byName or byKey must be set")
         }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as ResponderID
+
+        if (byName != other.byName) return false
+        if (byKey != null) {
+            if (other.byKey == null) return false
+            if (!byKey.contentEquals(other.byKey)) return false
+        } else if (other.byKey != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = byName?.hashCode() ?: 0
+        result = 31 * result + (byKey?.contentHashCode() ?: 0)
+        return result
+    }
+
+    companion object : Asn1Decodable<Asn1ExplicitlyTagged, ResponderID> {
+
+        override fun doDecode(src: Asn1ExplicitlyTagged): ResponderID = src.decodeRethrowing {
+            when (src.tag.tagValue) {
+                1uL -> ResponderID(
+                    byName = next().asSequence().children.map {
+                        RelativeDistinguishedName.decodeFromTlv(it.asSet())
+                    },
+                    byKey = null
+                )
+                2uL -> ResponderID(
+                    byName = null,
+                    byKey = next().asPrimitive().decode(Asn1Element.Tag.OCTET_STRING) { it }
+                )
+                else -> throw Asn1StructuralException("Invalid tag for ResponderID: ${src.tag}")
+            }
+        }
+    }
+}
+
+data class ResponseData(
+    val version: Int? = 2,
+    val responderID: ResponderID,
+    val producedAt: Asn1Time,
+    val responses: List<SingleResponse>,
+    val responsesExtensions: List<X509CertificateExtension>? = null
+) : Asn1Encodable<Asn1Sequence> {
+
+    override fun encodeToTlv(): Asn1Sequence = Asn1.Sequence{
+        version?.let { Asn1.ExplicitlyTagged(Tags.VERSION.tagValue) { +Asn1.Int(version) } }
+        +responderID
         +producedAt
         +Asn1.Sequence { responses.forEach { +it } }
         responsesExtensions?.let {
@@ -172,35 +222,6 @@ data class ResponseData(
         }
     }
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || this::class != other::class) return false
-
-        other as ResponseData
-
-        if (version != other.version) return false
-        if (byName != other.byName) return false
-        if (byKey != null) {
-            if (other.byKey == null) return false
-            if (!byKey.contentEquals(other.byKey)) return false
-        } else if (other.byKey != null) return false
-        if (producedAt != other.producedAt) return false
-        if (responses != other.responses) return false
-        if (responsesExtensions != other.responsesExtensions) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = version ?: 0
-        result = 31 * result + (byName?.hashCode() ?: 0)
-        result = 31 * result + (byKey?.contentHashCode() ?: 0)
-        result = 31 * result + producedAt.hashCode()
-        result = 31 * result + responses.hashCode()
-        result = 31 * result + (responsesExtensions?.hashCode() ?: 0)
-        return result
-    }
-
     companion object : Asn1Decodable<Asn1Sequence, ResponseData> {
 
         object Tags {
@@ -208,46 +229,24 @@ data class ResponseData(
             val EXTENSIONS = Asn1.ExplicitTag(1uL)
         }
 
-        override fun doDecode(src: Asn1Sequence): ResponseData {
-            val version = src.peek().let {
+        override fun doDecode(src: Asn1Sequence): ResponseData = src.decodeRethrowing {
+            val version = peek().let {
                 if (it is Asn1ExplicitlyTagged && runCatching { it.verifyTag(Tags.VERSION) }.isSuccess) {
                     it.asPrimitive().decodeToInt()
-                        .also { src.nextChild() }
+                        .also { next() }
                 } else {
                     null
                 }
             }
 
-            val responderId = src.nextChild()
-            val byName: List<RelativeDistinguishedName>?
-            val byKey: ByteArray?
-            when (responderId) {
-                is Asn1ExplicitlyTagged -> {
-                    when (responderId.tag.tagValue) {
-                        1uL -> {
-                            byName = responderId.nextChild().asSequence().children.map {
-                                RelativeDistinguishedName.decodeFromTlv(it.asSet())
-                            }
-                            byKey = null
-                        }
-                        2uL -> {
-                            byKey = responderId.nextChild().asPrimitive().decode(
-                                Asn1Element.Tag.OCTET_STRING
-                            ) { it }
-                            byName = null
-                        }
-                        else -> throw Asn1StructuralException("Invalid tag for ResponderID: ${responderId.tag}")
-                    }
-                }
-                else -> throw Asn1StructuralException("ResponderID must be explicitly tagged")
-            }
+            val responderId = ResponderID.decodeFromTlv(next().asExplicitlyTagged())
 
-            val time = Asn1Time.decodeFromTlv(src.nextChild().asPrimitive())
+            val time = Asn1Time.decodeFromTlv(next().asPrimitive())
 
-            val responses = src.nextChild().asSequence().children.map { SingleResponse.decodeFromTlv(it.asSequence()) }
+            val responses = next().asSequence().children.map { SingleResponse.decodeFromTlv(it.asSequence()) }
 
-            val extensions = if (src.hasMoreChildren()) {
-                src.nextChild().asExplicitlyTagged().verifyTag(Tags.EXTENSIONS.tagValue)
+            val extensions = if (hasNext()) {
+                next().asExplicitlyTagged().verifyTag(Tags.EXTENSIONS.tagValue)
                     .single().asSequence().children.map {
                         X509CertificateExtension.decodeFromTlv(it.asSequence())
                     }
@@ -255,8 +254,7 @@ data class ResponseData(
 
             return ResponseData(
                 version,
-                byName,
-                byKey,
+                responderId,
                 time,
                 responses,
                 extensions
@@ -292,12 +290,12 @@ data class BasicOCSPResponse(
         object Tags {
             val CERTS = Asn1.ExplicitTag(0uL)
         }
-        override fun doDecode(src: Asn1Sequence): BasicOCSPResponse {
-            val responseData = ResponseData.decodeFromTlv(src.nextChild().asSequence())
-            val alg = X509SignatureAlgorithm.decodeFromTlv(src.nextChild().asSequence())
-            val signature = CryptoSignature.fromX509Encoded(alg, src.nextChild().asPrimitive())
-            val certs = if (src.hasMoreChildren()) {
-                src.nextChild().asExplicitlyTagged().verifyTag(Tags.CERTS.tagValue)
+        override fun doDecode(src: Asn1Sequence): BasicOCSPResponse = src.decodeRethrowing {
+            val responseData = ResponseData.decodeFromTlv(next().asSequence())
+            val alg = X509SignatureAlgorithm.decodeFromTlv(next().asSequence())
+            val signature = CryptoSignature.fromX509Encoded(alg, next().asPrimitive())
+            val certs = if (hasNext()) {
+                next().asExplicitlyTagged().verifyTag(Tags.CERTS.tagValue)
                     .single().asSequence().children.map {
                         X509Certificate.decodeFromTlv(it.asSequence())
                     }
@@ -325,11 +323,11 @@ data class ResponseBytes(
 
     companion object : Asn1Decodable<Asn1Sequence, ResponseBytes> {
 
-        override fun doDecode(src: Asn1Sequence): ResponseBytes {
-            val oid = ObjectIdentifier.decodeFromTlv(src.nextChild().asPrimitive())
+        override fun doDecode(src: Asn1Sequence): ResponseBytes = src.decodeRethrowing {
+            val oid = ObjectIdentifier.decodeFromTlv(next().asPrimitive())
             if (oid != KnownOIDs.ocspBasic) throw Asn1StructuralException("Invalid Basic OCSP Response.")
 
-            val response = BasicOCSPResponse.decodeFromDer(src.nextChild().asOctetString().content)
+            val response = BasicOCSPResponse.decodeFromDer(next().asOctetString().content)
             return ResponseBytes(oid, response)
         }
 
@@ -384,10 +382,10 @@ data class OCSPResponse(
             val RESPONSE_BYTES = Asn1.ExplicitTag(0uL)
         }
 
-        override fun doDecode(src: Asn1Sequence): OCSPResponse {
-            val status = OCSPResponseStatus.parseStatus(src.nextChild().asPrimitive())
-            val response = if (src.hasMoreChildren())
-                ResponseBytes.decodeFromTlv(src.nextChild().asExplicitlyTagged().verifyTag(Tags.RESPONSE_BYTES.tagValue).single().asSequence())
+        override fun doDecode(src: Asn1Sequence): OCSPResponse = src.decodeRethrowing {
+            val status = OCSPResponseStatus.parseStatus(next().asPrimitive())
+            val response = if (hasNext())
+                ResponseBytes.decodeFromTlv(next().asExplicitlyTagged().verifyTag(Tags.RESPONSE_BYTES.tagValue).single().asSequence())
             else null
 
             return OCSPResponse(status, response)
@@ -399,9 +397,9 @@ data class OCSPResponse(
          * (`-----BEGIN OCSP RESPONSE-----`) and then decoding from Base64.
          */
         fun decodeFromByteArray(src: ByteArray): OCSPResponse? = catchingUnwrapped {
-            OCSPResponse.decodeFromTlv(Asn1Element.parse(src) as Asn1Sequence)
+            OCSPResponse.decodeFromTlv(Asn1Element.parse(src).asSequence())
         }.getOrNull() ?: catchingUnwrapped {
-            OCSPResponse.decodeFromTlv(Asn1Element.parse(src.decodeToByteArray(Base64())) as Asn1Sequence)
+            OCSPResponse.decodeFromTlv(Asn1Element.parse(src.decodeToByteArray(Base64())).asSequence())
         }.getOrNull() ?: OCSPResponse.decodeFromPem(src.decodeToString()).getOrNull()
     }
 }
