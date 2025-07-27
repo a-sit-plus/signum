ffdb1304d1c6f3f52123da90dab0c427bd0d4fa9
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-07-27 18:43:55 +0200

    add ASP repo + update gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven("https://central.sonatype.com/repository/maven-snapshots/")
-        maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
         maven {
             url = uri("https://raw.githubusercontent.com/a-sit-plus/gradle-conventions-plugin/mvn/repo")
             name = "aspConventions"
@@ -17,16 +16,9 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         mavenLocal()
-    }
-}
-
-gradle.allprojects {
-    // run after each project is evaluated so that the plugin has
-    // already added whatever it wanted to add
-    afterEvaluate {
-        repositories.removeIf { repo ->
-            repo is MavenArtifactRepository &&
-                    repo.url.toString().contains("https://s01.oss.sonatype.org/content/repositories")
+        maven {
+            url = uri("https://raw.githubusercontent.com/a-sit-plus/gradle-conventions-plugin/mvn/repo")
+            name = "aspConventions"
         }
     }
 }
