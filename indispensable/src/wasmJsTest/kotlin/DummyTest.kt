7e5e928adf87299d6d1ea4d80fa177921fa02867
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-07-28 00:41:17 +0200

    fix wasmjs dummy test

--- indispensable/src/wasmJsTest/kotlin/DummyTest.kt
+++ indispensable/src/wasmJsTest/kotlin/DummyTest.kt
@@ -1,4 +1,9 @@
+package at.asitplus.signum.test
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldNotBe
 
-class DummyTest: FreeSpec( {
+class WasmJsTest: FreeSpec( {
+  "This dummy test" {
+      "is t jut making sure" shouldNotBe "that WasmJS tests are indeed running"
+  }
 })
