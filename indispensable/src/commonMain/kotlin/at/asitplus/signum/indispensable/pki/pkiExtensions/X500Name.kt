82d4100a8f7c24c00df7710f4bd9ca791588c629
Srdjan Stjepanovic <srdjan.stjepanovic@a-sit.at>
2025-09-09 11:24:21 +0200

    resolve bugs raised by cherry-pick

--- indispensable/src/commonMain/kotlin/at/asitplus/signum/indispensable/pki/pkiExtensions/X500Name.kt
+++ indispensable/src/commonMain/kotlin/at/asitplus/signum/indispensable/pki/pkiExtensions/X500Name.kt
@@ -2,13 +2,15 @@ package at.asitplus.signum.indispensable.pki.pkiExtensions
 
 import at.asitplus.signum.indispensable.asn1.Asn1Decodable
 import at.asitplus.signum.indispensable.asn1.Asn1Encodable
+import at.asitplus.signum.indispensable.asn1.Asn1Primitive
 import at.asitplus.signum.indispensable.asn1.Asn1Sequence
-import at.asitplus.signum.indispensable.asn1.Asn1Set
+import at.asitplus.signum.indispensable.asn1.decodeRethrowing
 import at.asitplus.signum.indispensable.asn1.encoding.Asn1
-import at.asitplus.signum.indispensable.asn1.runRethrowing
+import at.asitplus.signum.indispensable.asn1.encoding.asAsn1String
+import at.asitplus.signum.indispensable.pki.AttributeTypeAndValue
 import at.asitplus.signum.indispensable.pki.RelativeDistinguishedName
 
-class X500Name(
+data class X500Name(
     val relativeDistinguishedNames: List<RelativeDistinguishedName>,
     override val type: GeneralNameOption.NameType = GeneralNameOption.NameType.DIRECTORY
 ) : Asn1Encodable<Asn1Sequence>, GeneralNameOption {
@@ -18,15 +20,18 @@ class X500Name(
     }
 
     companion object : Asn1Decodable<Asn1Sequence, X500Name> {
-        override fun doDecode(src: Asn1Sequence): X500Name = runRethrowing {
-            X500Name(src.children.map { RelativeDistinguishedName.decodeFromTlv(it as Asn1Set) })
+        override fun doDecode(src: Asn1Sequence): X500Name = src.decodeRethrowing {
+            buildList {
+                while (hasNext()) {
+                    add(RelativeDistinguishedName.decodeFromTlv(next().asSet()))
+                }
+            }.let(::X500Name)
         }
-
     }
 
     override fun toString() = "X500Name(RDNs=${relativeDistinguishedNames.joinToString()})"
 
-    override fun constraints(input: GeneralNameOption?): GeneralNameOption.ConstraintResult {
+    override fun constrains(input: GeneralNameOption?): GeneralNameOption.ConstraintResult {
         if (input !is X500Name) return GeneralNameOption.ConstraintResult.DIFF_TYPE
 
         if (this == input) return GeneralNameOption.ConstraintResult.MATCH
@@ -53,5 +58,34 @@ class X500Name(
         }
 
         return true
+    }
+
+    fun findMostSpecificCommonName(): AttributeTypeAndValue.CommonName? {
+        for (rdn in relativeDistinguishedNames.asReversed()) {
+            for (attr in rdn.attrsAndValues) {
+                if (attr is AttributeTypeAndValue.CommonName) {
+                    return attr
+                }
+            }
+        }
+        return null
+    }
+
+    fun toRfc2253String(): String =
+        relativeDistinguishedNames
+            .asReversed()
+            .joinToString(",") { rdn ->
+                rdn.attrsAndValues.joinToString("+") { atv ->
+                    "${atv.oidToString()}=${(atv.value as? Asn1Primitive)?.asAsn1String()?.value}"
+                }
+            }
+
+    private fun AttributeTypeAndValue.oidToString(): String = when (oid) {
+        AttributeTypeAndValue.CommonName.OID -> "CN"
+        AttributeTypeAndValue.Organization.OID -> "O"
+        AttributeTypeAndValue.OrganizationalUnit.OID -> "OU"
+        AttributeTypeAndValue.Country.OID -> "C"
+        AttributeTypeAndValue.EmailAddress.OID -> "EMAILADDRESS"
+        else -> oid.toString()
     }
 }
