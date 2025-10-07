3d87dc39b431990f321ae920581d8653f4027f95
Srdjan Stjepanovic <srdjan.stjepanovic@a-sit.at>
2025-10-07 10:49:30 +0200

    remove performValidation property

--- indispensable/src/commonMain/kotlin/at/asitplus/signum/indispensable/pki/generalNames/RegisteredIDName.kt
+++ indispensable/src/commonMain/kotlin/at/asitplus/signum/indispensable/pki/generalNames/RegisteredIDName.kt
@@ -8,7 +8,6 @@ import at.asitplus.signum.indispensable.asn1.ObjectIdentifier
 
 data class RegisteredIDName internal constructor(
     val value: ObjectIdentifier,
-    override val performValidation: Boolean = false,
     override val type: GeneralNameOption.NameType = GeneralNameOption.NameType.OID
 ): GeneralNameOption, Asn1Encodable<Asn1Primitive> {
 
