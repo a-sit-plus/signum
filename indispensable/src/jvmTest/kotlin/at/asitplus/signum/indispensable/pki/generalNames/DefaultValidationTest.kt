09d37e46f4d2894cfc6726d23bb43a3999dcc02e
Srdjan Stjepanovic <srdjan.stjepanovic@a-sit.at>
2025-10-15 12:08:11 +0200

    add tests for custom logic in unvalidated generalNames

---
+++ indispensable/src/jvmTest/kotlin/at/asitplus/signum/indispensable/pki/generalNames/DefaultValidationTest.kt
@@ -0,0 +1,40 @@
+package at.asitplus.signum.indispensable.pki.generalNames
+
+import at.asitplus.signum.indispensable.asn1.Asn1Element
+import at.asitplus.signum.indispensable.asn1.encoding.parse
+import at.asitplus.test.FreeSpec
+import io.kotest.matchers.shouldBe
+
+class DefaultValidationTest : FreeSpec ({
+    val bytes = byteArrayOf(-96, 30, 6, 3, 42, 3, 4, -96, 23, 12, 21, 115, 111, 109, 101, 32, 111, 116, 104, 101, 114, 32, 105, 100, 101, 110, 116, 105, 102, 105, 101, 114)
+
+    "EDIPartyName without custom validation " {
+        val value = Asn1Element.parse(bytes).asExplicitlyTagged()
+
+        val name = EDIPartyName(value)
+        name.isValid shouldBe null
+
+        val validated = name.createValidatedCopy { true }
+        validated.isValid shouldBe true
+    }
+
+    "OtherName without custom validation " {
+        val value = Asn1Element.parse(bytes).asExplicitlyTagged()
+
+        val name = OtherName(value)
+        name.isValid shouldBe null
+
+        val validated = name.createValidatedCopy { true }
+        validated.isValid shouldBe true
+    }
+
+    "X400AddressName without custom validation " {
+        val value = Asn1Element.parse(bytes)
+
+        val name = X400AddressName(value)
+        name.isValid shouldBe null
+
+        val validated = name.createValidatedCopy { true }
+        validated.isValid shouldBe true
+    }
+})
