5a78208b761e42fb1e19f85c2b514fdbea9d5efc
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-10-17 13:27:50 +0200

    last fixes

--- indispensable/src/commonTest/kotlin/SecureRandomTest.kt
+++ indispensable/src/commonTest/kotlin/SecureRandomTest.kt
@@ -1,31 +1,38 @@
 import at.asitplus.signum.SecureRandom
+import at.asitplus.test.Target
 import at.asitplus.testballoon.withData
 import de.infix.testBalloon.framework.testSuite
 import io.kotest.matchers.ints.shouldBeGreaterThanOrEqual
 import io.kotest.matchers.shouldBe
+import kotlin.random.Random
+
 
 val SecureRandomTest by testSuite {
+
+    //This is precisely why we need a Random-compatible SecureRandom
+    var random: Random = when (Target.current) {
+        Target.JS_BROWSER, Target.JS_NODE, Target.JS_WEBWORKER, Target.JS_GENERIC, Target.WASMJS -> Random.Default
+        else -> SecureRandom
+    }
+
     withData(List(33) { it }) { bitCount ->
         // 0 bits: always zero; nothing to loop for
         if (bitCount == 0) {
-            val rnd = SecureRandom.nextBits(0)
+            val rnd = random.nextBits(0)
             rnd shouldBe 0
             return@withData
         }
 
         // Always: upper bits must be zero
-        val rnd1 = SecureRandom.nextBits(bitCount)
+        val rnd1 = random.nextBits(bitCount)
         rnd1.countLeadingZeroBits() shouldBeGreaterThanOrEqual (32 - bitCount)
-
 
         // Loop until the highest of the produced bits is set (bitCount-1)
         val targetMask = 1 shl (bitCount - 1)
         var rnd = rnd1
-        println("First Try: $rnd")
-        var trials =1uL
+        var trials = 1uL
         while ((rnd and targetMask) == 0) {
-            rnd = SecureRandom.nextBits(bitCount)
-            println("Trial ${trials++}: $rnd")
+            rnd = random.nextBits(bitCount)
             rnd.countLeadingZeroBits() shouldBeGreaterThanOrEqual (32 - bitCount)
         }
     }
