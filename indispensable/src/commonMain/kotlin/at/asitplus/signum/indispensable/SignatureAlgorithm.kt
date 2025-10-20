f96b262430be21521694f18cc1c5ae22e1224cdd
Srdjan Stjepanovic <srdjan.stjepanovic@a-sit.at>
2025-10-20 12:01:57 +0200

    fix bug in HMAC and SignatureAlgorithm

--- indispensable/src/commonMain/kotlin/at/asitplus/signum/indispensable/SignatureAlgorithm.kt
+++ indispensable/src/commonMain/kotlin/at/asitplus/signum/indispensable/SignatureAlgorithm.kt
@@ -1,6 +1,7 @@
 package at.asitplus.signum.indispensable
 
 import at.asitplus.signum.Enumeration
+import at.asitplus.signum.indispensable.SignatureAlgorithm.Companion
 
 enum class RSAPadding {
     PKCS1,
@@ -14,42 +15,57 @@ sealed interface SignatureAlgorithm: DataIntegrityAlgorithm {
         val digest: Digest?,
         /** Whether this algorithm specifies a particular curve to use, or `null` for any curve. */
         val requiredCurve: ECCurve?
-    ) : SignatureAlgorithm
+    ) : SignatureAlgorithm {
+
+        companion object : Enumeration<ECDSA> {
+            val ECDSAwithSHA256 = ECDSA(Digest.SHA256, null)
+            val ECDSAwithSHA384 = ECDSA(Digest.SHA384, null)
+            val ECDSAwithSHA512 = ECDSA(Digest.SHA512, null)
+
+            override val entries: Set<ECDSA> by lazy {
+                setOf(
+                    ECDSAwithSHA256,
+                    ECDSAwithSHA384,
+                    ECDSAwithSHA512
+                )
+            }
+        }
+    }
 
     data class RSA(
         /** The digest to apply to the data. */
         val digest: Digest,
         /** The padding to apply to the data. */
         val padding: RSAPadding
-    ) : SignatureAlgorithm
+    ) : SignatureAlgorithm {
+
+        companion object : Enumeration<RSA> {
+            val RSAwithSHA256andPKCS1Padding = RSA(Digest.SHA256, RSAPadding.PKCS1)
+            val RSAwithSHA384andPKCS1Padding = RSA(Digest.SHA384, RSAPadding.PKCS1)
+            val RSAwithSHA512andPKCS1Padding = RSA(Digest.SHA512, RSAPadding.PKCS1)
+
+            val RSAwithSHA256andPSSPadding = RSA(Digest.SHA256, RSAPadding.PSS)
+            val RSAwithSHA384andPSSPadding = RSA(Digest.SHA384, RSAPadding.PSS)
+            val RSAwithSHA512andPSSPadding = RSA(Digest.SHA512, RSAPadding.PSS)
+            override val entries: Set<RSA> by lazy {
+                setOf(
+                    RSAwithSHA256andPSSPadding,
+                    RSAwithSHA384andPSSPadding,
+                    RSAwithSHA512andPSSPadding,
+
+                    RSAwithSHA256andPKCS1Padding,
+                    RSAwithSHA384andPKCS1Padding,
+                    RSAwithSHA512andPKCS1Padding
+                )
+            }
+
+        }
+    }
 
     companion object : Enumeration<SignatureAlgorithm> {
-        val ECDSAwithSHA256 = ECDSA(Digest.SHA256, null)
-        val ECDSAwithSHA384 = ECDSA(Digest.SHA384, null)
-        val ECDSAwithSHA512 = ECDSA(Digest.SHA512, null)
-
-        val RSAwithSHA256andPKCS1Padding = RSA(Digest.SHA256, RSAPadding.PKCS1)
-        val RSAwithSHA384andPKCS1Padding = RSA(Digest.SHA384, RSAPadding.PKCS1)
-        val RSAwithSHA512andPKCS1Padding = RSA(Digest.SHA512, RSAPadding.PKCS1)
-
-        val RSAwithSHA256andPSSPadding = RSA(Digest.SHA256, RSAPadding.PSS)
-        val RSAwithSHA384andPSSPadding = RSA(Digest.SHA384, RSAPadding.PSS)
-        val RSAwithSHA512andPSSPadding = RSA(Digest.SHA512, RSAPadding.PSS)
 
         override val entries: Set<SignatureAlgorithm> by lazy {
-            setOf(
-                ECDSAwithSHA256,
-                ECDSAwithSHA384,
-                ECDSAwithSHA512,
-
-                RSAwithSHA256andPSSPadding,
-                RSAwithSHA384andPSSPadding,
-                RSAwithSHA512andPSSPadding,
-
-                RSAwithSHA256andPKCS1Padding,
-                RSAwithSHA384andPKCS1Padding,
-                RSAwithSHA512andPKCS1Padding
-            )
+            ECDSA.entries + RSA.entries
         }
 
     }
