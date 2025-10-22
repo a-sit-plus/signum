339cec0c02611dc8e75efceb3e39d4c687dbd83f
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-10-22 13:13:30 +0200

    fix testballoon override

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,13 @@ import java.time.Duration
 
 plugins {
     val kotlinVer = System.getenv("KOTLIN_VERSION_ENV")?.ifBlank { null } ?: libs.versions.kotlin.get()
+    val testballoonVer = System.getenv("TESTBALLOON_VERSION_OVERRIDE")?.ifBlank { null } ?: libs.versions.testballoon.get()
 
     alias(libs.plugins.asp)
     kotlin("multiplatform") version kotlinVer apply false
     kotlin("plugin.serialization") version kotlinVer apply false
     id("com.android.kotlin.multiplatform.library") version libs.versions.agp.get() apply (false)
-    id("de.infix.testBalloon") version libs.versions.testballoon.get() apply false
+    id("de.infix.testBalloon") version testballoonVer apply false
 }
 group = "at.asitplus.signum"
 subprojects {
