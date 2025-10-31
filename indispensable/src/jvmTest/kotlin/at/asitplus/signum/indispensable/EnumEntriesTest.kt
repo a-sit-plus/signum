4349f208739ef34a5eaab18773a536a8108f3391
Jakob Heher <jakob.heher@iaik.tugraz.at>
2025-10-31 13:52:50 +0100

    Revert "have tests actually run"
    This reverts commit 338f40915c3811622e43abaaee081996e237c4d1.

--- indispensable/src/jvmTest/kotlin/at/asitplus/signum/indispensable/EnumEntriesTest.kt
+++ indispensable/src/jvmTest/kotlin/at/asitplus/signum/indispensable/EnumEntriesTest.kt
@@ -3,8 +3,6 @@ package at.asitplus.signum.indispensable
 import at.asitplus.signum.Enumerable
 import at.asitplus.signum.Enumeration
 import at.asitplus.signum.test.enumConsistencyTest
-import de.infix.testBalloon.framework.TestDiscoverable
-import de.infix.testBalloon.framework.testSuite
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.nulls.shouldNotBeNull
@@ -13,6 +11,4 @@ import io.kotest.matchers.types.shouldBeInstanceOf
 import kotlin.reflect.KClass
 import kotlin.reflect.full.companionObject
 
-val EnumEntriesTest by testSuite {
-    enumConsistencyTest("IndispensableEnums")
-}
+val EnumEntriesTest by enumConsistencyTest("IndispensableEnums")
