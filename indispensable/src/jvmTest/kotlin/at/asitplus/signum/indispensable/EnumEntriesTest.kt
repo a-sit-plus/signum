cea08e3be4b05fc59deaa9d132458241cb1ea0aa
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-10-31 06:44:26 +0100

    have tests actually run

--- indispensable/src/jvmTest/kotlin/at/asitplus/signum/indispensable/EnumEntriesTest.kt
+++ indispensable/src/jvmTest/kotlin/at/asitplus/signum/indispensable/EnumEntriesTest.kt
@@ -3,6 +3,8 @@ package at.asitplus.signum.indispensable
 import at.asitplus.signum.Enumerable
 import at.asitplus.signum.Enumeration
 import at.asitplus.signum.test.enumConsistencyTest
+import de.infix.testBalloon.framework.TestDiscoverable
+import de.infix.testBalloon.framework.testSuite
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.nulls.shouldNotBeNull
@@ -11,4 +13,6 @@ import io.kotest.matchers.types.shouldBeInstanceOf
 import kotlin.reflect.KClass
 import kotlin.reflect.full.companionObject
 
-val EnumEntriesTest by enumConsistencyTest("IndispensableEnums")
+val EnumEntriesTest by testSuite {
+    enumConsistencyTest("IndispensableEnums")
+}
