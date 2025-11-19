e067cbef6ed60c2ec725171400f6373246181bb5
Srdjan Stjepanovic <srdjan.stjepanovic@a-sit.at>
2025-11-19 11:43:07 +0100

    polishing

--- supreme/src/commonTest/kotlin/at/asitplus/signum/supreme/validate/NameConstraintsTest.kt
+++ supreme/src/commonTest/kotlin/at/asitplus/signum/supreme/validate/NameConstraintsTest.kt
@@ -1,5 +1,6 @@
 package at.asitplus.signum.supreme.validate
 
+import at.asitplus.signum.ExperimentalPkiApi
 import at.asitplus.signum.indispensable.pki.CertificateChain
 import at.asitplus.signum.indispensable.pki.X509Certificate
 import at.asitplus.signum.indispensable.pki.validate.NameConstraintsValidator
@@ -8,9 +9,10 @@ import at.asitplus.testballoon.invoke
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 
-/*
-* PKITS 4.13 Name Constraints
-* */
+/**
+ * PKITS 4.13 Name Constraints
+ */
+@OptIn(ExperimentalPkiApi::class)
 val NameConstraintsTest by testSuite {
 
     val trustAnchorRootCertificate = "-----BEGIN CERTIFICATE-----\n" +
