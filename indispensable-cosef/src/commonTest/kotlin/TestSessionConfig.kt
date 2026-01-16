002637d20be787847c228af53c5bac12e27ccbac
Bernd Prünster <bernd.pruenster@a-sit.at>
2026-01-16 08:22:33 +0100

    unlimited test name length

--- indispensable-cosef/src/commonTest/kotlin/TestSessionConfig.kt
+++ indispensable-cosef/src/commonTest/kotlin/TestSessionConfig.kt
@@ -1,3 +1,6 @@
+import at.asitplus.testballoon.DataTest
+import at.asitplus.testballoon.FreeSpec
+import at.asitplus.testballoon.PropertyTest
 import de.infix.testBalloon.framework.core.TestConfig
 import de.infix.testBalloon.framework.core.TestSession
 import de.infix.testBalloon.framework.core.invocation
@@ -8,4 +11,13 @@ import kotlin.time.Duration.Companion.minutes
 class ModuleTestSession : TestSession(
     testConfig = DefaultConfiguration.invocation(TestConfig.Invocation.Concurrent)
         .testScope(isEnabled = false, timeout = 20.minutes)
-)
+) {
+    init {
+        DataTest.defaultTestNameMaxLength = -1
+        DataTest.defaultDisplayNameMaxLength = -1
+        FreeSpec.defaultTestNameMaxLength = -1
+        FreeSpec.defaultDisplayNameMaxLength = -1
+        PropertyTest.defaultTestNameMaxLength = -1
+        PropertyTest.defaultDisplayNameMaxLength = -1
+    }
+}
--- supreme/src/commonTest/kotlin/TestSessionConfig.kt
+++ supreme/src/commonTest/kotlin/TestSessionConfig.kt
@@ -1,4 +1,5 @@
 import at.asitplus.testballoon.DataTest
+import at.asitplus.testballoon.FreeSpec
 import at.asitplus.testballoon.PropertyTest
 import de.infix.testBalloon.framework.core.TestConfig
 import de.infix.testBalloon.framework.core.TestSession
@@ -10,10 +11,16 @@ import kotlin.time.Duration.Companion.minutes
 class ModuleTestSession : TestSession(
     testConfig = DefaultConfiguration.invocation(TestConfig.Invocation.Concurrent)
         .testScope(isEnabled = false, timeout = 20.minutes)
-){
+) {
     init {
         //Don't make the heap explode
         DataTest.compactByDefault = true
         PropertyTest.compactByDefault = true
+        DataTest.defaultTestNameMaxLength = -1
+        DataTest.defaultDisplayNameMaxLength = -1
+        FreeSpec.defaultTestNameMaxLength = -1
+        FreeSpec.defaultDisplayNameMaxLength = -1
+        PropertyTest.defaultTestNameMaxLength = -1
+        PropertyTest.defaultDisplayNameMaxLength = -1
     }
 }
