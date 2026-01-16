a9f31ba44a4a1bb5e633f7b44f496b9ac7c53587
Bernd Prünster <bernd.pruenster@a-sit.at>
2026-01-16 07:28:06 +0100

    disable test scopes

--- indispensable-asn1/src/commonTest/kotlin/TestSessionConfig.kt
+++ indispensable-asn1/src/commonTest/kotlin/TestSessionConfig.kt
@@ -11,7 +11,7 @@ import kotlin.time.Duration.Companion.minutes
 //Supercharge tests with concurrency
 class ModuleTestSession : TestSession(
     testConfig = DefaultConfiguration.invocation(TestConfig.Invocation.Concurrent)
-        .testScope(isEnabled = true, timeout = 20.minutes)
+        .testScope(isEnabled = false, timeout = 20.minutes)
 ){
     init {
         //Don't make the heap explode
--- indispensable-josef/src/commonTest/kotlin/TestSessionConfig.kt
+++ indispensable-josef/src/commonTest/kotlin/TestSessionConfig.kt
@@ -8,5 +8,5 @@ import kotlin.time.Duration.Companion.minutes
 //Supercharge tests with concurrency!
 class ModuleTestSession : TestSession(
     testConfig = DefaultConfiguration.invocation(TestConfig.Invocation.Concurrent)
-        .testScope(isEnabled = true, timeout = 20.minutes)
+        .testScope(isEnabled = false, timeout = 20.minutes)
 )
