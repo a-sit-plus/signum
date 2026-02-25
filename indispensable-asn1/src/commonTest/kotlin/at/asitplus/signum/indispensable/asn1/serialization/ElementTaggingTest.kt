f1b261d5f3172c14a96b93900f2c2ee216474deb
Bernd Prünster <bernd.pruenster@a-sit.at>
2026-02-25 15:58:17 +0100

    cleanup

--- indispensable-asn1/src/commonTest/kotlin/at/asitplus/signum/indispensable/asn1/serialization/ElementTaggingTest.kt
+++ indispensable-asn1/src/commonTest/kotlin/at/asitplus/signum/indispensable/asn1/serialization/ElementTaggingTest.kt
@@ -1,6 +1,5 @@
 package io.kotest.property.at.asitplus.signum.indispensable.asn1.serialization
 
-import Asn1Backed
 import at.asitplus.signum.indispensable.asn1.Asn1Element
 import at.asitplus.signum.indispensable.asn1.Asn1Integer
 import at.asitplus.signum.indispensable.asn1.serialization.*
@@ -62,24 +61,6 @@ val TaggedTest by testSuite {
                 decoded.rawValue.tag.tagValue shouldBe 9uL
             }
         }
-
-        "Asn1BackedImplicitlyTagged" {
-            DER.encodeToDer(Asn1BackedImplicitlyTagged(int)).toHexString() shouldBe "300389010$int".also {
-                DER.decodeFromDer<Asn1BackedImplicitlyTagged>(it.hexToByteArray()).also { decoded ->
-                    decoded.rawValue.asn1Element!!.tag.tagValue shouldBe 9uL
-                } shouldBe Asn1BackedImplicitlyTagged(
-                    int
-                )
-            }
-
-            withClue("missing implicit tag, default int tag") {
-                //As you can see, this throws, because the int was not implicitly tagged. so ALL the validations are done
-                //as if there were no Asn1Backed in place
-                shouldThrow<SerializationException> {
-                    DER.decodeFromDer<Asn1BackedImplicitlyTagged>("300302010$int".hexToByteArray())
-                }
-            }
-        }
     }
 }
 
@@ -112,24 +93,6 @@ data class ImplicitlyTaggedElement private constructor(@Asn1Tag(9u) private val 
     //For this simple example this is not an issue, because there is such an int decoding function, but imagine we don't have int, but TbsCertificate, whose
     //raw ASN.1 representation is required (And yes, we want ASN.1 that round-trip deserializes and serializes to bytes, so we have structural guarantees; hence: raw bytes are not an option)
     val value = DER.decodeFromTlv<Int>(rawValue)
-}
-
-
-@Serializable
-data class Asn1BackedImplicitlyTagged private constructor(@Asn1Tag(9u) val rawValue: Asn1Backed<Int>) {
-    //The encoding path also works fine. default it serializer, no manual parsing or custom serializer required
-
-    //The deserialization codepath performs all the exact same checks as with a regular plain int and gives the ras ASN.1 element in addtion to the
-    //decoded value. So if decoding fails we get an exception. if it works, we have the guarantee that the value is structurally a correct representation of the underlying type
-
-    // at the same time the asn.1 bytes are not touched, because TLV will be preserved byte for byte when re-encoding and TLV knnows nothing about booleans, ints, etc.
-
-    //moreover: Asn1Backed is completely invisible to other serializazion formats, so I can even debug this to JSON
-    constructor(value: Int) : this(Asn1Backed(value))
-
-    @Transient
-    //This also works perfectly fine and we can encode and decode as often as we want: If an Asn1Backed's asn1Element is present, it can just be re-emitted as-is, to keep byte-level faults that don't impact the TLV tree (like sorting issues, or illegal characters inside a string, etc.)
-    val value = rawValue.value
 }
 
 @Asn1Tag(9u)
