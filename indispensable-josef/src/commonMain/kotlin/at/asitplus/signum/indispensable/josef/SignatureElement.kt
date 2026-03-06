c0305fdff57ef1c6a7ca0634d15c204ed8eae8aa
Simon Mueller <simon.mueller@a-sit.at>
2026-03-06 16:22:01 +0100

    Update SignatureElement

--- indispensable-josef/src/commonMain/kotlin/at/asitplus/signum/indispensable/josef/SignatureElement.kt
+++ indispensable-josef/src/commonMain/kotlin/at/asitplus/signum/indispensable/josef/SignatureElement.kt
@@ -1,21 +1,30 @@
 package at.asitplus.signum.indispensable.josef
 
 import at.asitplus.signum.indispensable.CryptoSignature
+import at.asitplus.signum.indispensable.io.ByteArrayBase64UrlSerializer
+import at.asitplus.signum.indispensable.josef.io.joseCompliantSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationException
 import kotlinx.serialization.Transient
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.decodeFromJsonElement
 
 
 /**
  * Signature element as defined in [RFC 7515 Sec 7.2.1](https://www.rfc-editor.org/rfc/rfc7515.html#section-7.2.1)
- *
- * DOES NOT IMPLEMENT UNPROTECTED HEADERS!
- * Will only be implemented when use-case arises as this significantly
- * impacts JWS data class representation and verification
- * (The header in JWS is the union of protected and unprotected header elements)
  */
-//@Serializable(with = SignatureElementSerializer::class)
+@Serializable
 data class SignatureElement(
+    /**
+     * The [plainSignature] member MUST be present
+     *
+     * Serialization: BASE64URL(JWS Signature).
+     */
+    @SerialName(JWS.SerialNames.SIGNATURE)
+    @Serializable(ByteArrayBase64UrlSerializer::class)
+    val plainSignature: ByteArray,
+
     /**
      * The [protectedHeader] member MUST be present ...when the JWS Protected
      * Header value is non-empty; otherwise, it MUST be absent.  These
@@ -23,45 +32,49 @@ data class SignatureElement(
      *
      * Serialization: BASE64URL(UTF8(JWS Protected Header))
      */
-    @SerialName(SerialNames.PROTECTED)
-    @Serializable(with = JwsProtectedHeaderSerializer::class)
-    val protectedHeader: JwsHeader,
+    @SerialName(JWS.SerialNames.PROTECTED)
+    @Serializable(with = ByteArrayBase64UrlSerializer::class)
+    val plainProtectedHeader: ByteArray? = null,
 
-    /**
-     * The [signature] member MUST be present
-     *
-     * Serialization: BASE64URL(JWS Signature).
-     */
-    @SerialName(SerialNames.SIGNATURE)
-    val signature: CryptoSignature.RawByteEncodable,
-
-    /**
-     * ASCII string `<BASE64URL(protected)>` as used for signature verification.
-     * See first part of [JwsSigned.prepareJwsSignatureInput]
-     *
-     * This parameter is required for correct serialization!
-     */
-    @Transient
-    val plainHeaderInput: ByteArray,
+    @SerialName(JWS.SerialNames.HEADER)
+    val unprotectedHeader: JsonObject? = null
 ) {
-
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other == null || this::class != other::class) return false
 
         other as SignatureElement
 
-        if (protectedHeader != other.protectedHeader) return false
-        if (signature != other.signature) return false
-
+        if (!plainSignature.contentEquals(other.plainSignature)) return false
+        if (!plainProtectedHeader.contentEquals(other.plainProtectedHeader)) return false
         return true
     }
 
     override fun hashCode(): Int {
-        var result = protectedHeader.hashCode()
-        result = 31 * result + signature.hashCode()
+        var result = plainSignature.contentHashCode()
+        result = 31 * result + plainProtectedHeader.contentHashCode()
         return result
     }
 
+    @Transient
+    val protectedHeader: JsonObject? =
+        plainProtectedHeader?.let { joseCompliantSerializer.decodeFromString(it.decodeToString()) }
 
+    /**
+     * Only the combined header must be a valid [JwsHeader]
+     */
+    @Transient
+    val combinedHeader: JwsHeader =
+        joseCompliantSerializer.decodeFromJsonElement(unprotectedHeader.strictUnion(protectedHeader))
+
+    /**
+     * Lenient Signature Parsing
+     */
+    @Transient
+    val signature: CryptoSignature.RawByteEncodable
+        get() = when (val alg = combinedHeader.algorithm) {
+            is JwsAlgorithm.Signature.EC -> CryptoSignature.EC.fromRawBytes(alg.ecCurve, plainSignature)
+            is JwsAlgorithm.Signature.RSA -> CryptoSignature.RSA(plainSignature)
+            else -> throw SerializationException("Unsupported algorithm for JWS signature element: $alg")
+        }
 }
