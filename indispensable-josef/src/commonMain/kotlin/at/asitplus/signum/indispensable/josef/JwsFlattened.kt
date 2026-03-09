b55e475e0f00c68562bbf33c1bb21906aa2140cd
Simon Mueller <simon.mueller@a-sit.at>
2026-03-09 17:42:24 +0100

    Add sealed class JWS serializer

--- indispensable-josef/src/commonMain/kotlin/at/asitplus/signum/indispensable/josef/JwsFlattened.kt
+++ indispensable-josef/src/commonMain/kotlin/at/asitplus/signum/indispensable/josef/JwsFlattened.kt
@@ -2,24 +2,28 @@ package at.asitplus.signum.indispensable.josef
 
 import at.asitplus.signum.indispensable.contentEqualsIfArray
 import at.asitplus.signum.indispensable.io.ByteArrayBase64UrlSerializer
+import at.asitplus.signum.indispensable.josef.io.joseCompliantSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.decodeFromJsonElement
 
 
+@Serializable
 data class JwsFlattened(
     @Serializable(ByteArrayBase64UrlSerializer::class)
-    @SerialName(JWS.SerialNames.PROTECTED)
+    @SerialName(SerialNames.PROTECTED)
     val plainProtectedHeader: ByteArray? = null,
-    @SerialName(JWS.SerialNames.HEADER)
+    @SerialName(SerialNames.HEADER)
     val unprotectedHeader: JsonObject? = null,
     @Serializable(ByteArrayBase64UrlSerializer::class)
-    @SerialName(JWS.SerialNames.PAYLOAD)
+    @SerialName(SerialNames.PAYLOAD)
     override val payload: ByteArray,
     @Serializable(ByteArrayBase64UrlSerializer::class)
-    @SerialName(JWS.SerialNames.SIGNATURE)
+    @SerialName(SerialNames.SIGNATURE)
     val plainSignature: ByteArray
 ) : JWS() {
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other == null || this::class != other::class) return false
@@ -40,6 +44,24 @@ data class JwsFlattened(
         result = 31 * result + payload.contentHashCode()
         result = 31 * result + plainSignature.contentHashCode()
         return result
+    }
+
+    companion object {
+        fun invoke(
+            protectedHeader: JsonObject,
+            unprotectedHeader: JsonObject,
+            payload: ByteArray,
+            signer: (JwsAlgorithm, ByteArray) -> ByteArray
+        ): JwsFlattened {
+            val jwsHeader = joseCompliantSerializer.decodeFromJsonElement<JwsHeader>(protectedHeader.strictUnion(unprotectedHeader))
+            val plainProtectedHeader = joseCompliantSerializer.encodeToString(protectedHeader).encodeToByteArray()
+            return JwsFlattened(
+                plainProtectedHeader = plainProtectedHeader,
+                unprotectedHeader = unprotectedHeader,
+                payload = payload,
+                plainSignature = signer(jwsHeader.algorithm, getSignatureInput(plainProtectedHeader, payload))
+            )
+        }
     }
 }
 
