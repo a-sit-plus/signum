b1a4e51917b9298cd83f2d04e1e9edcb41905cd6
Simon Mueller <simon.mueller@a-sit.at>
2026-06-15 15:07:01 +0200

    Refactor JwtPayload

--- indispensable-josef/src/commonMain/kotlin/at/asitplus/signum/indispensable/josef/jwtpayload/JwtPayload.kt
+++ indispensable-josef/src/commonMain/kotlin/at/asitplus/signum/indispensable/josef/jwtpayload/JwtPayload.kt
@@ -7,9 +7,9 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 
 /**
- * Marker interface identifying data classes which are valid JWT-Payloads.
+ * Marker class identifying data classes which are valid JWT-Payloads.
  *
- * This interface does not declare any functionality and exists solely for
+ * This class does not declare any functionality and exists solely for
  * type identification.
  */
 abstract class JwtPayload(
